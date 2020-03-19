fixed more normalization bugs

src/datasets/cifar.py
@@ -7,13 +7,13 @@ from os import path
 class Cifar(Dataset):
     def __init__(self, batch_size, data_location=path.join(path.dirname(__file__), "../../data"),
                  download=False, shuffle=True, version="cifar10"):
-        super().__init__(batch_size, [
+        super().__init__(batch_size)
+        if version not in ["cifar10", "cifar100"]:
+            raise ValueError("version must be in {cifar10, cifar100}")
+        transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
-        if version not in ["cifar10", "cifar100"]:
-            raise ValueError("version must be in {cifar10, cifar100}")
-        transform = transforms.Compose(self.transforms)
         ds_constructors = {"cifar10": datasets.CIFAR10, "cifar100": datasets.CIFAR100}
         self.train_loader = torch.utils.data.DataLoader(
             ds_constructors[version](data_location, train=True, download=download, transform=transform),

src/datasets/dataset.py
@@ -1,12 +1,12 @@
 from typing import Iterable, Tuple
+from torchvision import transforms
 import torch
 
 
 class Dataset:
-    def __init__(self, batch_size, transforms):
+    def __init__(self, batch_size):
         self.batch_size = batch_size
         self.mask_value = 0.
-        self.transforms = transforms
 
     def get_train_data(self) -> Iterable:
         raise NotImplementedError
@@ -19,12 +19,3 @@ class Dataset:
 
     def get_batch_size(self) -> int:
         return self.batch_size
-
-    def transform_batch(self, batch):
-        result = []
-        for i in range(batch.shape[0]):
-            transformed = batch[i]
-            for t in self.transforms:
-                transformed = t(transformed)
-            result.append(transformed)
-        return torch.cat(result, dim=0)

src/datasets/perturbed_image_dataset.py
@@ -66,6 +66,7 @@ class PerturbedImageDataset:
             # [batch_size, *sample_shape], [batch_size]
             samples, labels = batch
             batch_size = samples.shape[0]
+            channels = samples.shape[1]
             # Convert to numpy if necessary
             if type(samples) == torch.Tensor:
                 samples = samples.numpy()
@@ -80,7 +81,14 @@ class PerturbedImageDataset:
                 batch = []
                 # Perturb and check if output hasn't changed
                 for p_l in perturbation_levels:
-                    perturbed_samples = dataset.transform_batch(p_fns[perturbation_fn](samples, p_l))
+                    perturbed_samples = p_fns[perturbation_fn](samples, p_l)
+
+                    im_mean = perturbed_samples.reshape((batch_size, channels, -1)).mean(2)\
+                        .reshape((batch_size, channels, 1, 1))
+                    im_std = perturbed_samples.reshape((batch_size, channels, -1)).std(2)\
+                        .reshape((batch_size, channels, 1, 1))
+                    perturbed_samples = (perturbed_samples - im_mean) / im_std
+
                     predictions = model.predict(torch.tensor(perturbed_samples))
                     batch_ok = batch_ok and not torch.any(predictions.argmax(axis=1) != torch.tensor(labels))
                     batch.append(perturbed_samples)

src/lib/report.py
@@ -39,7 +39,7 @@ class Report:
         pass
 
     @staticmethod
-    def _plot_images_row(imgs, width=200, height=200, dw=1, dh=1):
+    def _plot_images_row(imgs):
         if type(imgs) == torch.Tensor:
             imgs = imgs.detach().numpy()
         is_grayscale = imgs.shape[1] == 1
@@ -48,7 +48,7 @@ class Report:
         for i in range(n_imgs):
             img = imgs[i]
             # Convert range to 0..255 uint32
-            img = (img - np.min(imgs))/(np.max(imgs) - np.min(imgs))
+            img = (img - np.min(img))/(np.max(img) - np.min(img))
             img *= 255
             img = np.array(img, dtype=np.uint32)
 
@@ -64,10 +64,10 @@ class Report:
             img = np.flip(img, axis=0)
             concat_imgs.append(img)
         concat_imgs = np.concatenate(concat_imgs, axis=-1)
-        p = plotting.figure(width=width*n_imgs, height=height)
+        p = plotting.figure(width=200*n_imgs, height=200)
         p.toolbar_location = None
         p.axis.visible = False
         p.grid.visible = False
         plot_fn = p.image if is_grayscale else p.image_rgba
-        plot_fn([concat_imgs], x=0, y=0, dw=dw, dh=dh)
+        plot_fn([concat_imgs], x=0, y=0, dw=1, dh=1)
         return p

src/scripts/perturbation_invariance.py
@@ -4,12 +4,11 @@ from methods import *
 from lib import Report
 import numpy as np
 import torch
-import itertools
 
-GENERATE = False
+GENERATE = True
 DATA_ROOT = "../../data"
 DATASET = "MNIST"
-PERT_FN = "mean_shift"
+PERT_FN = "noise"
 MODEL = "CNN"
 BATCH_SIZE = 4
 N_BATCHES = 16  # 128
@@ -26,7 +25,7 @@ if GENERATE:
     dataset = dataset_constructor(batch_size=BATCH_SIZE, download=False, shuffle=True)
     perturbed_dataset = PerturbedImageDataset.generate(DATA_ROOT, dataset_name, dataset, model,
                                                        perturbation_fn=PERT_FN,
-                                                       perturbation_levels=np.linspace(-1, 1, 10),
+                                                       perturbation_levels=np.linspace(0, 2, 10),
                                                        n_batches=N_BATCHES)
 else:
     perturbed_dataset = PerturbedImageDataset(DATA_ROOT, dataset_name, BATCH_SIZE)