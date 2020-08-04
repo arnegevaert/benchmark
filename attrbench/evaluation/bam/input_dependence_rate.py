finished MCS script + fixed bug in masking

attrbench/evaluation/bam/input_dependence_rate.py
@@ -13,26 +13,26 @@ def input_dependence_rate(dataloader: Iterable, model: Callable, methods: Dict[s
     is removed. 1 - IDR can be interpreted as "false positive rate": rate of explanations
     that assign higher importance to less important features.
     """
-    result = {m_name: {"overlay": [], "scene": []} for m_name in methods}
+    result = {m_name: {"object_attrs": [], "scene_attrs": [], "mask_size": []} for m_name in methods}
     for images, scenes, masks, scene_labels, object_labels in tqdm(dataloader):
         images = images.to(device)
         scenes = scenes.to(device)
         labels = scene_labels.to(device)
-        masks = masks.to(device)  # [batch_size, 1, height, width]
+        masks = masks.squeeze().to(device)  # [batch_size, 1, height, width]
         with torch.no_grad():
             y_pred = torch.argmax(model(images), dim=1)
         # Boolean array indicating correctly classified images
         correctly_classified = (y_pred == labels)
         for m_name in methods:
-            attrs_overlay = methods[m_name](images, labels)  # [batch_size, *sample_shape]
-            attrs_scene = methods[m_name](scenes, labels)  # [batch_size, *sample_shape]
-            masked_attrs_overlay = masks * attrs_overlay
-            masked_attrs_scene = masks * attrs_scene
+            object_attrs = methods[m_name](images, labels)  # [batch_size, *sample_shape]
+            scene_attrs = methods[m_name](scenes, labels)  # [batch_size, *sample_shape]
             mask_size = torch.sum(masks.flatten(1), dim=1)  # [batch_size]
-            avg_attrs_overlay = masked_attrs_overlay.flatten(1).sum(dim=1) / mask_size
-            avg_attrs_scene = masked_attrs_scene.flatten(1).sum(dim=1) / mask_size
-            result[m_name]["overlay"].append(avg_attrs_overlay[correctly_classified].cpu().detach().numpy())
-            result[m_name]["scene"].append(avg_attrs_scene[correctly_classified].cpu().detach().numpy())
-    return {m_name: {"overlay": np.concatenate(result[m_name]["overlay"]),
-                     "scene": np.concatenate(result[m_name]["scene"])}
+            masked_object_attrs = (masks * object_attrs).flatten(1).sum(dim=1)
+            result[m_name]["object_attrs"].append(masked_object_attrs[correctly_classified].cpu().detach().numpy())
+            masked_scene_attrs = (masks * scene_attrs).flatten(1).sum(dim=1)
+            result[m_name]["scene_attrs"].append(masked_scene_attrs[correctly_classified].cpu().detach().numpy())
+            result[m_name]["mask_size"].append(mask_size[correctly_classified].cpu().detach().numpy())
+    return {m_name: {"object_attrs": np.concatenate(result[m_name]["object_attrs"]),
+                     "scene_attrs": np.concatenate(result[m_name]["scene_attrs"]),
+                     "mask_size": np.concatenate(result[m_name]["mask_size"])}
             for m_name in methods}

attrbench/evaluation/bam/model_contrast_score.py
@@ -17,7 +17,7 @@ def model_contrast_score(dataloader: Iterable, object_methods: Dict[str, Callabl
     for images, masks, _, labels in tqdm(dataloader):
         images = images.to(device)
         labels = labels.to(device)
-        masks = masks.to(device)
+        masks = masks.squeeze().to(device)
         for m_name in object_methods:
             object_model_attrs = object_methods[m_name](images, labels)
             scene_model_attrs = scene_methods[m_name](images, labels)
@@ -29,5 +29,5 @@ def model_contrast_score(dataloader: Iterable, object_methods: Dict[str, Callabl
             result[m_name]["mask_size"].append(mask_size.cpu().detach().numpy())
     return {m_name: {"object_attrs": np.concatenate(result[m_name]["object_attrs"]),
                      "scene_attrs": np.concatenate(result[m_name]["scene_attrs"]),
-                     "mask_size": np.concatenate(result[m_name]["masks"])}
+                     "mask_size": np.concatenate(result[m_name]["mask_size"])}
             for m_name in object_methods}

scripts/bam/input_dependence_rate.py
@@ -28,7 +28,7 @@ parser.add_argument("--aggregation-fn", type=str, choices=["avg", "max-abs"], de
 parser.add_argument("--cuda", type=bool, default=True)
 parser.add_argument("--data-root", type=str, default="../../data")
 parser.add_argument("--experiment-name", type=str, default="experiment")
-parser.add_argument("--out-dir", type=str, default="../../out/BAM/IDR")
+parser.add_argument("--out-dir", type=str, default=".")
 args = parser.parse_args()
 device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
 
@@ -60,10 +60,10 @@ kwargs = {
 }
 
 attribution_methods = {
-    #"Gradient": attribution.Gradient(model, **kwargs),
-    "SmoothGrad": attribution.SmoothGrad(model, **kwargs),
-    #"InputXGradient": attribution.InputXGradient(model, **kwargs),
-    "IntegratedGradients": attribution.IntegratedGradients(model, **kwargs),
+    "Gradient": attribution.Gradient(model, **kwargs),
+    #"SmoothGrad": attribution.SmoothGrad(model, **kwargs),
+    "InputXGradient": attribution.InputXGradient(model, **kwargs),
+    #"IntegratedGradients": attribution.IntegratedGradients(model, **kwargs),
     #"GuidedBackprop": attribution.GuidedBackprop(model, **kwargs),
     #"Deconvolution": attribution.Deconvolution(model, **kwargs),
     #"Ablation": attribution.Ablation(model, **kwargs),

scripts/bam/model_contrast_score.py
@@ -29,7 +29,7 @@ parser.add_argument("--aggregation-fn", type=str, choices=["avg", "max-abs"], de
 parser.add_argument("--cuda", type=bool, default=True)
 parser.add_argument("--data-root", type=str, default="../../data")
 parser.add_argument("--experiment-name", type=str, default="experiment")
-parser.add_argument("--out-dir", type=str)
+parser.add_argument("--out-dir", type=str, default=".")
 args = parser.parse_args()
 device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
 
@@ -42,42 +42,45 @@ if path.isfile(path.join(args.out_dir, f"{args.experiment_name}.pkl")):
 dataset = BAMDataset(path.join(args.data_root, "BAM"), train=False, include_orig_scene=False, include_mask=True)
 dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
 
-import itertools
-dataloader = itertools.islice(iter(dataloader), 4)
-
 model_constructor = getattr(models, args.model_type)
 model_kwargs = {
-    "params_loc": args.model_params,
     "output_logits": args.use_logits,
     "num_classes": dataset.num_classes
 }
 if args.model_version:
     model_kwargs["version"] = args.model_version
-model = model_constructor(**model_kwargs)
-model.to(device)
-model.eval()
 
+object_model = model_constructor(**{**model_kwargs, **{"params_loc": args.object_model_params}})
+object_model.to(device)
+object_model.eval()
+
+scene_model = model_constructor(**{**model_kwargs, **{"params_loc": args.scene_model_params}})
+scene_model.to(device)
+scene_model.eval()
 
 kwargs = {
     "normalize": args.normalize_attrs,
     "aggregation_fn": args.aggregation_fn
 }
 
+
 attribution_methods = {
-    "Gradient": attribution.Gradient(model, **kwargs),
-    "SmoothGrad": attribution.SmoothGrad(model, **kwargs),
-    #"InputXGradient": attribution.InputXGradient(model, **kwargs),
-    #"IntegratedGradients": attribution.IntegratedGradients(model, **kwargs),
-    #"GuidedBackprop": attribution.GuidedBackprop(model, **kwargs),
-    #"Deconvolution": attribution.Deconvolution(model, **kwargs),
-    #"Ablation": attribution.Ablation(model, **kwargs),
-    #"GuidedGradCAM": attribution.GuidedGradCAM(model, model.get_last_conv_layer(), dataset.sample_shape[1:], **kwargs),
-    #"GradCAM": attribution.GradCAM(model, model.get_last_conv_layer(), dataset.sample_shape[1:], **kwargs)
+    "Gradient": lambda model: attribution.Gradient(model, **kwargs),
+    "SmoothGrad": lambda model: attribution.SmoothGrad(model, **kwargs),
+    #"InputXGradient": lambda model: attribution.InputXGradient(model, **kwargs),
+    #"IntegratedGradients": lambda model: attribution.IntegratedGradients(model, **kwargs),
+    #"GuidedBackprop": lambda model: attribution.GuidedBackprop(model, **kwargs),
+    #"Deconvolution": lambda model: attribution.Deconvolution(model, **kwargs),
+    #"Ablation": lambda model: attribution.Ablation(model, **kwargs),
+    #"GuidedGradCAM": lambda model: attribution.GuidedGradCAM(model, model.get_last_conv_layer(), dataset.sample_shape[1:], **kwargs),
+    #"GradCAM": lambda model: attribution.GradCAM(model, model.get_last_conv_layer(), dataset.sample_shape[1:], **kwargs)
 }
+object_methods = {m_name: attribution_methods[m_name](object_model) for m_name in attribution_methods}
+scene_methods = {m_name: attribution_methods[m_name](scene_model) for m_name in attribution_methods}
 
-result = input_dependence_rate(dataloader, model, attribution_methods, device)
+result = model_contrast_score(dataloader, object_methods, scene_methods, device)
+
 result_df = pd.concat([pd.DataFrame(result[m_name]).assign(method=m_name) for m_name in attribution_methods])
-
 result_df.to_pickle(path.join(args.out_dir, f"{args.experiment_name}.pkl"))
 meta_filename = path.join(args.out_dir, f"{args.experiment_name}_args.json")
 with open(meta_filename, "w") as f: