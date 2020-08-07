a3845ebf0b90d2388cc9a2188eb998e90f14c4e8
Arne Gevaert <arnegevaert@gmail.com>
2020-08-07 07:36:48 +0200

    better normalization

--- attrbench/evaluation/deletion_curves/deletion_curves.py
+++ attrbench/evaluation/deletion_curves/deletion_curves.py
@@ -38,31 +38,36 @@ def deletion_curves(data: Iterable, model: Callable, methods: Dict[str, Callable
             batch_result = np.concatenate(batch_result, axis=1)
             result[key].append(batch_result)
     # [n_batches*batch_size, len(mask_range)]
-    raw_data = {m_name: np.concatenate(result[m_name], axis=0) for m_name in methods}
-    return DeletionCurvesResult(raw_data=raw_data, x_range=mask_range), raw_data
+    raw_data = {
+        "x_range": mask_range,
+        "data": {
+            m_name: np.concatenate(result[m_name], axis=0) for m_name in methods
+        }
+    }
+    return DeletionCurvesResult(raw_data=raw_data), raw_data
 
 
 class DeletionCurvesResult:
-    def __init__(self, filename=None, raw_data=None, x_range=None):
+    def __init__(self, filename=None, raw_data=None):
         if not (raw_data or filename):
             raise ValueError("Must provide raw data dict or file name to load.")
         if raw_data:
             self.processed = {}
-            for method in raw_data:
-                normalized = raw_data[method] / np.expand_dims(raw_data[method][:, 0], -1)
+            self.x_range = raw_data["x_range"]
+            for method in raw_data["data"]:
+                normalized = raw_data["data"][method] / np.mean(raw_data["data"][method][:, 0])
+                sd = np.std(normalized, axis=0)
+                mean = np.mean(normalized, axis=0)
                 self.processed[method] = {
-                    "median": np.median(normalized, axis=0),
-                    "lower": np.percentile(normalized, 25, axis=0),
-                    "upper": np.percentile(normalized, 75, axis=0)
+                    "mean": mean,
+                    "lower": mean - (1.96 * sd / np.sqrt(normalized.shape[0])),
+                    "upper": mean + (1.96 * sd / np.sqrt(normalized.shape[0]))
                 }
-            if not x_range:
-                raise ValueError("Must provide x range when using raw data dict")
-            self.mask_range = x_range
         elif filename:
             with open(filename) as file:
                 contents = json.load(file)
                 data = contents["data"]
-                self.mask_range = contents["x_range"]
+                self.x_range = contents["x_range"]
                 self.processed = {
                     method: {
                         stat: np.array(data[method][stat]) for stat in data[method]
@@ -72,22 +77,22 @@ class DeletionCurvesResult:
     def plot(self, interval=False):
         fig, ax = plt.subplots(figsize=(7, 5))
         for method in self.processed:
-            ax.plot(self.mask_range, self.processed[method]["median"], label=method)
+            ax.plot(self.x_range, self.processed[method]["mean"], label=method)
             if interval:
-                ax.fill_between(x=self.mask_range, y1=self.processed[method]["lower"],
-                                y2=self.processed[method]["upper"], alpha=.1)
+                ax.fill_between(x=self.x_range, y1=self.processed[method]["lower"],
+                                y2=self.processed[method]["upper"], alpha=.2)
         ax.legend(loc=(0., 1.05), ncol=3)
         return fig, ax
 
     def auc(self):
         return {
-            method: np.mean(self.processed[method]["median"]) for method in self.processed
+            method: np.mean(self.processed[method]["mean"]) for method in self.processed
         }
 
     def save(self, filename):
         with open(filename, "w") as outfile:
             json.dump({
-                "x_range": self.mask_range,
+                "x_range": self.x_range,
                 "data": {
                     method: {
                         stat: self.processed[method][stat].tolist() for stat in self.processed[method]
