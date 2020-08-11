bug in masking code

attrbench/evaluation/insertion_deletion_curves/insertion_deletion_curves.py
@@ -34,52 +34,54 @@ def insertion_deletion_curves(data: Iterable, model: Callable, methods: Dict[str
         samples = samples[y_pred == labels]
         labels = labels[y_pred == labels]
         batch_size = samples.size(0)
-        for key in methods:
-            batch_result = []
-            attrs = methods[key](samples, labels)  # [batch_size, *sample_shape]
-            # Flatten each sample in order to sort indices per sample
-            attrs = attrs.flatten(1)  # [batch_size, -1]
-            # Sort indices of attrs in ascending order
-            sorted_indices = attrs.argsort().cpu().detach().numpy()
-            orig_predictions = _OUTPUT_TRANSFORMS[output_transform](model(samples))
-            orig_predictions = orig_predictions[np.arange(batch_size), labels].reshape(-1, 1)
+        color_channels = samples.size(1)
+        if batch_size > 0:
+            for key in methods:
+                batch_result = []
+                attrs = methods[key](samples, labels)  # [batch_size, *sample_shape]
+                # Flatten each sample in order to sort indices per sample
+                attrs = attrs.flatten(1)  # [batch_size, -1]
+                # Sort indices of attrs in ascending order
+                sorted_indices = attrs.argsort().cpu().detach().numpy()
+                orig_predictions = _OUTPUT_TRANSFORMS[output_transform](model(samples))
+                orig_predictions = orig_predictions[np.arange(batch_size), labels].reshape(-1, 1)
 
-            # Add first value to result (full mask or no mask)
-            if mode == "deletion":
-                batch_result.append(orig_predictions.cpu().detach().numpy())
-            else:
-                full_mask_predictions = full_mask_output[np.arange(batch_size), labels].reshape(-1, 1)
-                batch_result.append(full_mask_predictions.cpu().detach().numpy())
-
-            # Add partially masked values to result
-            for i in mask_range:
-                # Get indices of i most important inputs
-                masked_samples = samples.clone() if mode == "deletion" \
-                    else torch.ones(samples.shape).to(device) * mask_value
-                to_mask = sorted_indices[:, -i:]  # [batch-size, i]
-                batch_dim = np.column_stack([range(batch_size) for _ in range(i)])
-                if pixel_level_mask:
-                    unraveled = np.unravel_index(to_mask, samples.shape[2:])
-                    masked_samples[(batch_dim, -1, *unraveled)] = mask_value if mode == "deletion" \
-                        else samples[(batch_dim, -1, *unraveled)]
+                # Add first value to result (full mask or no mask)
+                if mode == "deletion":
+                    batch_result.append(orig_predictions.cpu().detach().numpy())
                 else:
-                    unraveled = np.unravel_index(to_mask, samples.shape[1:])
-                    masked_samples[(batch_dim, *unraveled)] = mask_value if mode == "deletion" \
-                        else samples[(batch_dim, *unraveled)]
-                # Get predictions for result
-                predictions = _OUTPUT_TRANSFORMS[output_transform](model(masked_samples))
-                predictions = predictions[np.arange(batch_size), labels].reshape(-1, 1)
-                batch_result.append(predictions.cpu().detach().numpy())
+                    full_mask_predictions = full_mask_output[np.arange(batch_size), labels].reshape(-1, 1)
+                    batch_result.append(full_mask_predictions.cpu().detach().numpy())
 
-            # Add final value to result (full mask or no mask)
-            if mode == "insertion":
-                batch_result.append(orig_predictions.cpu().detach().numpy())
-            else:
-                full_mask_predictions = full_mask_output[np.arange(batch_size), labels].reshape(-1, 1)
-                batch_result.append(full_mask_predictions.cpu().detach().numpy())
+                # Add partially masked values to result
+                for i in mask_range:
+                    # Get indices of i most important inputs
+                    masked_samples = samples.clone() if mode == "deletion" \
+                        else torch.ones(samples.shape).to(device) * mask_value
+                    to_mask = sorted_indices[:, -i:]  # [batch-size, i]
+                    batch_dim = np.column_stack([range(batch_size) for _ in range(i)])
+                    color_dim = np.column_stack([range(color_channels) for _ in range(i)])
+                    if pixel_level_mask:
+                        unraveled = np.unravel_index(to_mask, samples.shape[2:])
+                        masked_samples[(batch_dim, color_dim, *unraveled)] = mask_value if mode == "deletion" \
+                            else samples[(batch_dim, color_dim, *unraveled)]
+                    else:
+                        unraveled = np.unravel_index(to_mask, samples.shape[1:])
+                        masked_samples[(batch_dim, *unraveled)] = mask_value if mode == "deletion" \
+                            else samples[(batch_dim, *unraveled)]
+                    # Get predictions for result
+                    predictions = _OUTPUT_TRANSFORMS[output_transform](model(masked_samples))
+                    predictions = predictions[np.arange(batch_size), labels].reshape(-1, 1)
+                    batch_result.append(predictions.cpu().detach().numpy())
 
-            batch_result = np.concatenate(batch_result, axis=1)
-            result[key].append(batch_result)
+                # Add final value to result (full mask or no mask)
+                if mode == "insertion":
+                    batch_result.append(orig_predictions.cpu().detach().numpy())
+                else:
+                    full_mask_predictions = full_mask_output[np.arange(batch_size), labels].reshape(-1, 1)
+                    batch_result.append(full_mask_predictions.cpu().detach().numpy())
+                batch_result = np.concatenate(batch_result, axis=1)
+                result[key].append(batch_result)
     res_data = {
         # [n_batches*batch_size, len(mask_range)]
         m_name: np.concatenate(result[m_name], axis=0) for m_name in methods

scripts/insertion_deletion_curves.py
@@ -29,11 +29,11 @@ args = parser.parse_args()
 device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
 
 if path.isfile(args.output_file) and path.getsize(args.output_file) > 0:
-    exit("Experiment output file is not empty")
+    sys.exit("Experiment output file is not empty")
 
 with open(args.output_file, "w") as f:
     if not f.writable():
-        exit("Output file is not writable")
+        sys.exit("Output file is not writable")
 
 if args.dataset == "CIFAR10":
     dataset = datasets.Cifar(data_location=path.join(args.data_root, "CIFAR10"), train=False)