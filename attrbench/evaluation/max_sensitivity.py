1da47f7d4bb9e219fdcfa0fd351c4bf5a18a2745
Arne Gevaert <arnegevaert@gmail.com>
2020-10-12 21:30:22 +0200

    detach for memory saving

--- attrbench/evaluation/max_sensitivity.py
+++ attrbench/evaluation/max_sensitivity.py
@@ -10,13 +10,15 @@ def max_sensitivity(samples: torch.Tensor, labels: torch.Tensor, method: Callabl
     attrs = method(samples, labels)  # [batch_size, *sample_shape]
     norm = torch.norm(attrs.flatten(1), dim=1)
     for eps in perturbation_range:
+        print(f"eps: {eps}")
         diffs = []
         for _ in range(num_perturbations):
+            print("perturb")
             # Add uniform noise in [-eps, eps]
             noise = torch.rand(samples.shape, device=device) * 2 * eps - eps
             noisy_samples = samples + noise
             # Get new attributions from noisy samples
-            noisy_attrs = method(noisy_samples, labels)
+            noisy_attrs = method(noisy_samples, labels).detach()
             # Get relative norm of attribution difference
             # [batch_size]
             diffs.append(torch.norm(noisy_attrs.flatten(1) - attrs.flatten(1), dim=1) / norm)
@@ -25,4 +27,4 @@ def max_sensitivity(samples: torch.Tensor, labels: torch.Tensor, method: Callabl
         # [batch_size]
         result.append(diffs.max(dim=1)[0])
     # [batch_size, len(perturbation_range)]
-    return torch.stack(result, dim=1).cpu().detach()
+    return torch.stack(result, dim=1).cpu()
