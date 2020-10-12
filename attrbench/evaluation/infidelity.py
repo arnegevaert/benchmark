6bfe65061f72763e5f8eddf5f76bd7096f2e3b52
Arne Gevaert <arnegevaert@gmail.com>
2020-10-12 15:08:15 +0200

    fixed bug in infidelity

--- attrbench/evaluation/infidelity.py
+++ attrbench/evaluation/infidelity.py
@@ -10,24 +10,25 @@ def infidelity(samples: torch.Tensor, labels: torch.Tensor, model: Callable, met
     # Get original model output
     with torch.no_grad():
         orig_output = (model(samples)).gather(dim=1, index=labels.unsqueeze(-1))  # [batch_size, 1]
+    explanation = method(samples, labels)
+    # If explanation is on pixel level, we need to replicate value for each pixel n_channels times,
+    # since perturbations are [batch_size, n_channels, width, height]
+    if explanation.size(1) == 1:
+        explanation = explanation.repeat(1, n_channels, 1, 1)
 
     for eps in perturbation_range:
-        perturbation = torch.randn(samples.shape, device=device) * eps
-        perturbed = samples + perturbation  # [batch_size, n_channels, width, height]
-        with torch.no_grad():
-            perturbed_output = model(perturbed).gather(dim=1, index=labels.unsqueeze(-1))  # [batch_size, 1]
         eps_result = []
-        explanation = method(samples, labels)
-        # If explanation is on pixel level, we need to replicate value for each pixel n_channels times,
-        # since current_perturbation is [batch_size, n_channels, width, height]
-        if explanation.size(1) == 1:
-            explanation = explanation.repeat(1, n_channels, 1, 1)
         for _ in range(num_perturbations):
+            perturbation = torch.randn(samples.shape, device=device) * eps
+            perturbed = samples + perturbation  # [batch_size, n_channels, width, height]
+            with torch.no_grad():
+                perturbed_output = model(perturbed).gather(dim=1, index=labels.unsqueeze(-1))  # [batch_size, 1]
             # We calculate X * I (p3 in paper).
             # perturbation is \eps.
             # I = x - z_0, where z_0 = x_0 + \eps.
             # I = x - z_0 = x_0 - x_0 - \eps = -\eps.
             # I = -\eps = -perturbation
+            # Note: element-wise product followed by sum(dim=1) == diagonal of matmul (without doing entire matmul)
             dot_product = (explanation.flatten(1) * -perturbation.flatten(1)).sum(dim=1, keepdim=True)  # [batch_size, 1]
             eps_result.append((dot_product - (orig_output - perturbed_output))**2)  # [batch_size, 1]
         result.append(torch.stack(eps_result, dim=1).mean(dim=1))  # [batch_size]
