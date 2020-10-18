afd96a0e7e55dd70c0a970dcf9343fd2d1d78c63
Arne Gevaert <arnegevaert@gmail.com>
2020-10-18 17:27:13 +0200

    added impact coverage in plots

--- experiments/benchmark/plot_benchmark.py
+++ experiments/benchmark/plot_benchmark.py
@@ -5,19 +5,23 @@ from bokeh.plotting import figure, output_file, show
 from bokeh.layouts import column
 from bokeh.palettes import Category10_10 as palette
 from bokeh.models import Legend, Div
+import holoviews as hv
+hv.extension("bokeh")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dir", type=str)
+    parser.add_argument("dir", type=str)
     args = parser.parse_args()
 
-    all_metrics = ["insertion", "deletion", "infidelity", "max-sens", "s-impact", "impact", "sens-n"]
+    lineplots = ["insertion", "deletion", "infidelity", "max-sens", "s-impact", "impact", "sens-n"]
+    boxplots = ["i-coverage"]
 
+    # Create line plots
     result_data, meta = load_results(args.dir)
     output_file("report.html")
     figures = []
-    for metric in all_metrics:
+    for metric in lineplots:
         legend_data = []
         data = get_metric(result_data, metric)
         p = figure(title=metric, x_axis_label="x", y_axis_label="y", plot_width=1000, plot_height=500)
@@ -41,4 +45,17 @@ if __name__ == "__main__":
         legend.click_policy = "hide"
         p.add_layout(legend, "right")
         figures.append(p)
+
+    # Create box plots (impact coverage)
+    # TODO check if this plotting code is correct (compare to matplotlib)
+    renderer = hv.renderer("bokeh")
+    for metric in boxplots:
+        legend_data = []
+        data = get_metric(result_data, metric)
+        names = [method for method in data for _ in range(data[method].shape[0])]
+        plot_data = np.concatenate([data[method] for method in data])
+        boxwhisker = hv.BoxWhisker((names, plot_data), "Method", "Impact coverage", label=metric)
+        boxwhisker.opts(width=1000, height=500, xrotation=45)
+        figures.append(renderer.get_plot(boxwhisker).state)
+
     show(column([Div(text=f"<h1>Directory: {args.dir}</h1>")] + figures))
