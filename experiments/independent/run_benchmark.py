619f2abc2507d457fc5d9610ac43b90fa16af42e
Arne Gevaert <arnegevaert@gmail.com>
2020-11-25 14:49:00 +0100

    removed old code + fixed mem leak in sens-n

--- experiments/independent/run_benchmark.py
+++ experiments/independent/run_benchmark.py
@@ -9,46 +9,12 @@ import torch
 from tqdm import tqdm
 from os import path
 import os
-import json
 import warnings
 
 
-def logitsoftmax(logits):
-    b_size, n_classes = logits.size()
-    repeated = logits.unsqueeze(1).repeat(1, n_classes, 1)
-    idx = ~torch.eye(n_classes).unsqueeze(0).repeat(b_size, 1, 1).bool()
-    others = repeated[idx].view(b_size, n_classes, n_classes-1)
-    lse_others = torch.logsumexp(others, dim=-1)
-    return logits - lse_others
-
-def hard_lsm(logits):
-    b_size, n_classes = logits.size()
-    repeated = logits.unsqueeze(1).repeat(1, n_classes, 1)
-    idx = ~torch.eye(n_classes).unsqueeze(0).repeat(b_size, 1, 1).bool()
-    others = repeated[idx].view(b_size, n_classes, n_classes-1)
-    max_others = torch.max(others, dim=-1)
-    return logits - max_others
-
-class OutputTransformWrapper(torch.nn.Module):
-    def __init__(self, base_model, output_transform) -> None:
-        super().__init__()
-        self.base_model = base_model
-        transforms = {
-            "lsm": logitsoftmax,
-            "softmax": lambda x: torch.softmax(x, dim=-1),
-            "hard_lsm": hard_lsm
-        }
-        self.output_transform = transforms[output_transform]
-
-    def forward(self, x):
-        out = self.base_model(x)
-        return self.output_transform(out)
-    
-    def get_last_conv_layer(self):
-        return self.base_model.get_last_conv_layer()
-
-
 if __name__ == "__main__":
+    all_metrics = ["infidelity", "insertion", "deletion", "del-until-flip",
+                   "max-sens", "sens-n", "impact", "s-impact", "i-coverage"]
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dataset", type=str, required=True)
     parser.add_argument("-m", "--model", type=str, required=True)
@@ -57,61 +23,38 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--cuda", action="store_true")
     parser.add_argument("-o", "--out-dir", type=str, required=True)
     parser.add_argument("--methods", type=str, nargs="+", default=None)
-    parser.add_argument("--metrics", type=str, nargs="+", default=None)
+    parser.add_argument("--metrics", type=str, nargs="+", default=all_metrics, choices=all_metrics)
     parser.add_argument("--patch", type=str, default=None)
     parser.add_argument("--pixel-aggregation", type=str, choices=["avg", "max_abs"], default="avg")
-    parser.add_argument("--output-transform", type=str, choices=["softmax", "lsm", "hard_lsm"])
     parser.add_argument("--num-workers", type=int, default=4)
     # Parse arguments
     args = parser.parse_args()
-    if not path.isdir(args.out_dir):
-        os.makedirs(args.out_dir)
     device = "cuda" if torch.cuda.is_available() and args.cuda else "cpu"
+
+    # Create/check output directory
+    if not path.isdir(args.out_dir):
+        print(f"Created directory {args.out_dir}")
+        os.makedirs(args.out_dir)
+    if len(os.listdir(args.out_dir)) > 0:
+        warnings.warn(f"Directory {args.out_dir} is not empty")
 
     # Get model, dataset, methods, params
     dataset, model = get_ds_model(args.dataset, args.model)
-    if args.output_transform:
-        print(f"Using {args.output_transform}")
-        model = OutputTransformWrapper(model, args.output_transform)
     methods = get_methods(model, args.pixel_aggregation, normalize=False, methods=args.methods,
                           batch_size=args.batch_size, sample_shape=dataset.sample_shape[-2:])
-    masking_policy = PixelMaskingPolicy(mask_value=0.)
+    masking_policy = PixelMaskingPolicy(mask_value=0.)  # TODO configure this for different baselines
     model.to(device)
     for param in model.parameters():
         param.requires_grad = False
     model.eval()
-    #mask_range = get_mask_range(args.dataset)
     n_pixels = get_n_pixels(args.dataset)
-    step_size = int(0.02 * n_pixels)
-    mask_range = [step_size * i for i in range(25)]  # 0% to 50% of pixels in 25 steps
+    mask_range = list(np.rint(np.linspace(0, 1, 51) * n_pixels).astype(np.int))
     pert_range = list(np.linspace(.01, 2., 20))
-    metadata = {
-        "infidelity": {"x": pert_range},
-        "insertion": {"x": mask_range},
-        "deletion": {"x": mask_range},
-        "del-until-flip": {},
-        "max-sens": {"x": pert_range},
-        "sens-n": {"x": mask_range[1:]},
-        "impact": {"x": mask_range[1:]},
-        "s-impact": {"x": mask_range[1:]},
-        "i-coverage": {}
-    }
-
-    # Validate metrics argument
-    if args.metrics:
-        for m in args.metrics:
-            if m not in list(metadata.keys()):
-                raise ValueError(f"Unrecognized metric: {m}")
-    metrics = args.metrics if args.metrics else list(metadata.keys())
 
     # If no adversarial patch is given, we skip impact coverage
-    if "i-coverage" in metrics and not args.patch:
+    if "i-coverage" in args.metrics and not args.patch:
         warnings.warn("No adversarial patch found. Skipping impact coverage.")
-        metrics.remove("i-coverage")
-
-    # Save metadata
-    with open(path.join(args.out_dir, "meta.json"), "w") as fp:
-        json.dump({key: metadata[key] for key in metrics}, fp)
+        args.metrics.remove("i-coverage")
 
     # Run benchmark
     for i, m_name in enumerate(methods):
@@ -122,12 +65,7 @@ if __name__ == "__main__":
         if not path.isdir(subdir):
             os.makedirs(subdir)
         # Prepare results
-        res = {}
-        for name in metrics:
-            if name in ["impact", "s-impact"]:
-                res[name] = {"counts": [], "total": 0}
-            else:
-                res[name] = []
+        res = {name: [] for name in args.metrics}
 
         # Calculate metrics for each batch
         dataloader = iter(DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers))
@@ -146,28 +84,31 @@ if __name__ == "__main__":
                     diff = args.num_samples - samples_done
                     batch = batch[:diff]
                     labels = labels[:diff]
-                prog = tqdm(total=len(metrics), desc=f"{samples_done + 1}-{samples_done + batch.size(0)}/{args.num_samples}")
+                prog = tqdm(total=len(args.metrics), desc=f"{samples_done + 1}-{samples_done + batch.size(0)}/{args.num_samples}")
 
                 # Infidelity
-                if "infidelity" in metrics:
+                if "infidelity" in args.metrics:
                     prog.set_postfix({"metric": "Infidelity"})
-                    infid_method = DimReplication(method, 1, 3) if args.pixel_aggregation and batch.size(1) == 3 else method
+                    # Infidelity needs the attributions to have the same shape as the sample
+                    infid_method = method
+                    if args.pixel_aggregation and batch.size(1) == 3:
+                        infid_method = DimReplication(method, dim=1, amount=3)
                     batch_infidelity = infidelity(batch, labels, model, infid_method,
-                                                perturbation_range=pert_range,
-                                                num_perturbations=16)
+                                                  perturbation_range=pert_range,
+                                                  num_perturbations=16)
                     res["infidelity"].append(batch_infidelity)
                     prog.update(1)
 
                 # Insertion curves
-                if "insertion" in metrics:
+                if "insertion" in args.metrics:
                     prog.set_postfix({"metric": "Insertion"})
                     batch_insertion = insertion_deletion(batch, labels, model, method, mask_range,
-                                                        masking_policy, mode="insertion")
+                                                         masking_policy, mode="insertion")
                     res["insertion"].append(batch_insertion)
                     prog.update(1)
 
                 # Deletion curves
-                if "deletion" in metrics:
+                if "deletion" in args.metrics:
                     prog.set_postfix({"metric": "Deletion"})
                     batch_deletion = insertion_deletion(batch, labels, model, method, mask_range,
                                                         masking_policy, mode="deletion")
@@ -175,50 +116,52 @@ if __name__ == "__main__":
                     prog.update(1)
 
                 # Deletion until flip
-                if "del-until-flip" in metrics:
+                if "del-until-flip" in args.metrics:
                     prog.set_postfix({"metric": "Deletion until flip"})
                     batch_del_flip = deletion_until_flip(batch, labels, model, method, masking_policy,
-                                                        step_size=.01)
+                                                         step_size=.01)
                     res["del-until-flip"].append(batch_del_flip)
                     prog.update(1)
 
                 # Max-sensitivity
-                if "max-sens" in metrics:
+                if "max-sens" in args.metrics:
                     prog.set_postfix({"metric": "Max-sensitivity"})
                     batch_max_sens = max_sensitivity(batch, labels, method,
-                                                    perturbation_range=pert_range,
-                                                    num_perturbations=16)
+                                                     perturbation_range=pert_range,
+                                                     num_perturbations=16)
                     res["max-sens"].append(batch_max_sens)
                     prog.update(1)
 
                 # Sensitivity-n
-                if "sens-n" in metrics:
+                if "sens-n" in args.metrics:
                     prog.set_postfix({"metric": "Sensitivity-n"})
                     batch_sens_n = sensitivity_n(batch, labels, model, method, mask_range[1:],
-                                                num_subsets=100, masking_policy=masking_policy)
+                                                 num_subsets=100, masking_policy=masking_policy)
                     res["sens-n"].append(batch_sens_n)
                     prog.update(1)
 
                 # Impact score (non-strict)
-                if "impact" in metrics:
+                # Note: we can ignore the counts returned by (strict) impact score, since we already only use
+                # correctly classified samples (i.e. the count is always equal to batch size)
+                if "impact" in args.metrics:
                     prog.set_postfix({"metric": "Impact Score"})
-                    batch_impact_score, b_i_count = impact_score(batch, labels, model, mask_range[1:], method,
-                                                                masking_policy, tau=.5, strict=False)
-                    res["impact"]["counts"].append(batch_impact_score)
-                    res["impact"]["total"] += b_i_count
+                    batch_impact_score, _ = impact_score(batch, labels, model, mask_range[1:], method,
+                                                         masking_policy, tau=.5, strict=False)
+                    res["impact"].append(batch_impact_score)
                     prog.update(1)
 
                 # Impact score (strict)
-                if "s-impact" in metrics:
+                # Note: we can ignore the counts returned by (strict) impact score, since we already only use
+                # correctly classified samples (i.e. the count is always equal to batch size)
+                if "s-impact" in args.metrics:
                     prog.set_postfix({"metric": "Strict Impact Score"})
-                    batch_s_impact_score, b_s_i_count = impact_score(batch, labels, model, mask_range[1:], method,
-                                                                    masking_policy, strict=True)
-                    res["s-impact"]["counts"].append(batch_s_impact_score)
-                    res["s-impact"]["total"] += b_s_i_count
+                    batch_s_impact_score, _ = impact_score(batch, labels, model, mask_range[1:], method,
+                                                           masking_policy, strict=True)
+                    res["s-impact"].append(batch_s_impact_score)
                     prog.update(1)
 
                 # Impact coverage
-                if "i-coverage" in metrics:
+                if "i-coverage" in args.metrics:
                     prog.set_postfix({"metric": "Impact Coverage"})
                     patch = torch.load(args.patch)
                     iou, keep = impact_coverage(batch, labels, model, method, patch, target_label=0)
@@ -229,12 +172,26 @@ if __name__ == "__main__":
                 samples_done += batch.size(0)
 
         # Aggregate and save
-        for name in ["infidelity", "insertion", "deletion", "del-until-flip", "sens-n", "i-coverage", "max-sens"]:
-            if name in metrics:
-                res[name] = torch.cat(res[name], dim=0)
-                np.savetxt(path.join(subdir, f"{name}.csv"), res[name].numpy(), delimiter=',')
-        for name in ["impact", "s-impact"]:
-            if name in metrics:
-                res[name]["counts"] = torch.sum(torch.stack(res[name]["counts"], dim=0), dim=0).numpy().tolist()
-                with open(path.join(subdir, f"{name}.json"), "w") as fp:
-                    json.dump(res[name], fp)
+        headers = {
+            "infidelity": ','.join(pert_range),
+            "max-sens": ','.join(pert_range),
+            "insertion": ','.join(mask_range),
+            "deletion": ','.join(mask_range),
+            "sens-n": ','.join(mask_range[1:]),
+            "impact": ','.join(mask_range[1:]),
+            "s-impact": ','.join(mask_range[1:]),
+            "del-until-flip": None,
+            "i-coverage": None
+        }
+        for metric in args.metrics:
+            if metric in ["impact", "s-impact"]:
+                # Sum the counts and divide by total number of samples
+                np.savetxt(path.join(subdir, f"{metric}.csv"),
+                           (torch.sum(torch.stack(res[metric], dim=0), dim=0) / args.num_samples).numpy(),
+                           header=headers[metric],
+                           delimiter=',')
+            else:
+                np.savetxt(path.join(subdir, f"{metric}.csv"),
+                           torch.cat(res[metric], dim=0).numpy(),
+                           header=headers[metric],
+                           delimiter=',')
