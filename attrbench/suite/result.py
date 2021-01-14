252f34ec6e424b15832807929a78ed0c322cb63a
AxelJanRousseau <axeljanrousseau@gmail.com>
2021-01-14 10:46:26 +0100

    Dashboard (#78)
    * Add Save function to Result class
    
    Have suite use Result for saving data
    
    * Small changes
    
    * Cleanup debug code

--- attrbench/suite/result.py
+++ attrbench/suite/result.py
@@ -1,4 +1,4 @@
-from os import path
+from os import path, makedirs
 import pandas as pd
 import numpy as np
 import h5py
@@ -13,6 +13,40 @@ class Result:
         self.seed = seed
         self.images = images
         self.attributions = attributions
+
+    def save_hdf(self,filename):
+
+        # if dir not exists: create dir
+        if not path.exists(path.dirname(filename)) and path.dirname(filename) != '' :
+            makedirs(path.dirname(filename))
+
+        with h5py.File(filename, "w") as fp:
+            if self.seed is not None:
+                fp.attrs["seed"] = self.seed
+            if self.images is not None:
+                fp.create_dataset("images", data=self.images)
+            # Save attributions if specified
+            if self.attributions:
+                attr_group = fp.create_group("attributions")
+                for method_name in self.attributions.keys():
+                    attr_group.create_dataset(method_name, data=self.attributions[method_name])
+            # Save results
+            # results group is laid out as {metric}/{method}
+            # Each metric group has the according metadata as attributes
+            result_group = fp.create_group("results")
+            result_group.attrs["num_samples"] = self.num_samples
+
+            for metric_name in self.data.keys():
+                metric = self.data[metric_name]
+                metric_group = result_group.create_group(metric_name)
+
+                for attr_key, attr_value in self.metadata[metric_name].items():
+                    metric_group.attrs[attr_key] = attr_value
+
+                for method_name in metric.keys():
+                    method_results = metric[method_name]
+                    metric_group.create_dataset(method_name, data=method_results)
+
 
     @staticmethod
     def load_hdf(filename):
@@ -49,3 +83,4 @@ class Result:
 
     def get_methods(self):
         return list(self.data[self.get_metrics()[0]].keys())
+
