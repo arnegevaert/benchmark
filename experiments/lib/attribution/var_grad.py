be91cf12b445d1722cc5c90da8b94d0b5ebe6f48
AxelJanRousseau <axeljanrousseau@gmail.com>
2021-01-18 17:08:08 +0100

    add VarGrad

---
+++ experiments/lib/attribution/var_grad.py
@@ -0,0 +1,14 @@
+from captum import attr
+
+
+class VarGrad:
+    def __init__(self, model, num_samples, stdev):
+        self.method = attr.NoiseTunnel(attr.Saliency(model))
+        self.num_samples = num_samples
+        self.stdev = stdev
+
+    def __call__(self, x, target):
+        # sigma = self.noise_level / (x.max()-x.min()) # follows paper more closely, but not perfectly.
+        # in paper the sigma is set per image, here per batch
+        return self.method.attribute(x, target=target, nt_type="vargrad",
+                                     n_samples=self.num_samples, stdevs=self.stdev)
