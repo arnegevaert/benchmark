d65125cab0ec359863758654df872c6d01035812
AxelJanRousseau <axeljanrousseau@gmail.com>
2021-01-18 17:56:46 +0100

    Change to more recent version of Captum
    installed captum commit 6851f4c, This adds support for LRP, as well
    as support for internal batch size support for Noise Tunnel

--- experiments/lib/attribution/smooth_grad.py
+++ experiments/lib/attribution/smooth_grad.py
@@ -2,10 +2,11 @@ from captum import attr
 
 
 class SmoothGrad:
-    def __init__(self, model, num_samples, stdev):
+    def __init__(self, model, num_samples, stdev,internal_batch_size):
         self.method = attr.NoiseTunnel(attr.Saliency(model))
         self.num_samples = num_samples
         self.stdev = stdev
+        self.internal_batch_size = internal_batch_size
 
     def __call__(self, x, target):
         return self.method.attribute(x, target=target, nt_type="smoothgrad",
--- experiments/lib/attribution/var_grad.py
+++ experiments/lib/attribution/var_grad.py
@@ -2,13 +2,15 @@ from captum import attr
 
 
 class VarGrad:
-    def __init__(self, model, num_samples, stdev):
+    def __init__(self, model, num_samples, stdev, internal_batch_size):
         self.method = attr.NoiseTunnel(attr.Saliency(model))
         self.num_samples = num_samples
         self.stdev = stdev
+        self.internal_batch_size = internal_batch_size
 
     def __call__(self, x, target):
         # sigma = self.noise_level / (x.max()-x.min()) # follows paper more closely, but not perfectly.
         # in paper the sigma is set per image, here per batch
         return self.method.attribute(x, target=target, nt_type="vargrad",
-                                     n_samples=self.num_samples, stdevs=self.stdev)
+                                     nt_samples=self.num_samples,nt_samples_batch_size= self.internal_batch_size,
+                                     stdevs=self.stdev)
