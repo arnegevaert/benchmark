bd3a91fb593ed7c0aef12f280e56b59bf83195c4
Arne Gevaert <arnegevaert@gmail.com>
2021-01-27 16:31:24 +0100

    writer fix in del-until-flip

--- attrbench/functional/deletion_until_flip.py
+++ attrbench/functional/deletion_until_flip.py
@@ -7,9 +7,8 @@ import torch
 # a fully masked image (in which case we might not see a flip)
 def deletion_until_flip(samples: torch.Tensor, model: Callable, attrs: torch.Tensor,
                         num_steps: float, masking_policy: MaskingPolicy, writer=None):
-    debug_data = {}
     if writer is not None:
-        debug_data["flipped_samples"] = [None for _ in range(samples.shape[0])]
+        flipped_samples = [None for _ in range(samples.shape[0])]
         writer.add_images('Image samples', samples)
         writer.add_images('attributions', attrs)
     num_inputs = torch.prod(torch.tensor(attrs.shape[1:])).item()
@@ -41,11 +40,12 @@ def deletion_until_flip(samples: torch.Tensor, model: Callable, attrs: torch.Ten
         if writer is not None:
             for i in range(samples.shape[0]):
                 if flipped_this_iteration[i]:
-                    debug_data["flipped_samples"][i] = masked_samples[i]
+                    flipped_samples[i] = masked_samples[i].cpu()
         result[flipped_this_iteration] = mask_size
         flipped = new_flipped
     # Set maximum value for samples that were never flipped
     result[result == -1] = num_inputs
     if writer is not None:
-        writer.add_images('Flipped samples', torch.stack(debug_data["flipped_samples"]))
+        writer.add_images('Flipped samples', torch.stack(
+            [s if s is not None else torch.zeros(samples.shape[1:]) for s in flipped_samples]))
     return result
