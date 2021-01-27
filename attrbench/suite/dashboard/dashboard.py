4d63c66588d26563d35746e2461a0ab6217729be
Arne Gevaert <arnegevaert@gmail.com>
2021-01-27 11:04:27 +0100

    added normality test + replaced t-test with nonparametric wilcoxon test

--- attrbench/suite/dashboard/dashboard.py
+++ attrbench/suite/dashboard/dashboard.py
@@ -30,7 +30,8 @@ class Dashboard:
             "/clustering": (ClusteringPage(self.result_obj), "Clustering"),
             "/samples_attributions": (SamplesAttributionsPage(self.result_obj, self.app), "Samples/Attributions"),
             "/detail": (DetailPage(self.result_obj, self.app), "Detail"),
-            "/effect_size": (EffectSizePage(self.result_obj), "Effect Size")
+            "/effect_size": (EffectSizePage(self.result_obj), "Effect Size"),
+            "/normality": (NormalityPage(self.result_obj), "Normality")
         }
 
         self.root = "/overview"
