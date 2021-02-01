a227b61e2c1868a6a17e2c773f28f5e7e053d5ac
Arne Gevaert <arnegevaert@gmail.com>
2021-02-01 11:38:41 +0100

    same changes for random and blurring masker

--- attrbench/lib/masking/blurring_masker.py
+++ attrbench/lib/masking/blurring_masker.py
@@ -10,20 +10,24 @@ class BlurringMasker(Masker):
         self.kernel_size = kernel_size
 
     def mask(self, samples, indices):
-        res = []
+        batch_size, num_channels, rows, cols = samples.shape
+        num_indices = indices.shape[1]
+        batch_dim = np.tile(range(batch_size), (num_indices, 1)).transpose()
+
+        baseline = []
         for i in range(samples.shape[0]):
             sample = samples[i, ...]
             cv_sample = sample.permute(1, 2, 0).cpu().numpy()
             blurred_sample = torch.tensor(blur(cv_sample, (self.kernel_size, self.kernel_size)))
             if len(blurred_sample.shape) == 2:
                 blurred_sample = blurred_sample.unsqueeze(-1)
-            baseline = blurred_sample.permute(2, 0, 1).to(samples.device)
+            baseline.append(blurred_sample.permute(2, 0, 1).to(samples.device))
+        baseline = torch.stack(baseline, dim=0)
 
-            to_mask = torch.zeros(sample.shape).flatten(0 if self.feature_level == "channel" else 1).to(samples.device)
-            if self.feature_level == "channel":
-                to_mask[indices[i]] = 1.
-            else:
-                to_mask[:, indices[i]] = 1.
-            to_mask = to_mask.reshape(sample.shape)
-            res.append(sample - (to_mask * sample) + (to_mask * baseline))
-        return torch.stack(res, dim=0)
+        to_mask = torch.zeros(samples.shape, device=samples.device).flatten(1 if self.feature_level == "channel" else 2)
+        if self.feature_level == "channel":
+            to_mask[batch_dim, indices] = 1.
+        else:
+            to_mask[batch_dim, :, indices] = 1.
+        to_mask = to_mask.reshape(samples.shape)
+        return samples - (to_mask * samples) + (to_mask * baseline)
--- attrbench/lib/masking/random_masker.py
+++ attrbench/lib/masking/random_masker.py
@@ -1,28 +1,30 @@
 from attrbench.lib.masking import Masker
 import torch
+import numpy as np
 
 
 class RandomMasker(Masker):
-    def __init__(self, feature_level, additive=False, std=1, num_samples=10):
+    def __init__(self, feature_level, additive=False, std=1, num_samples=1):
         super().__init__(feature_level)
         self.additive = additive
         self.std = std
         self.num_samples = num_samples
 
     def mask(self, samples, indices):
-        res = []
-        for i in range(samples.shape[0]):
-            sample = samples[i, ...]
-            mean = sample if self.additive else torch.zeros(sample.shape, device=samples.device)
-            baseline = torch.normal(mean=mean, std=self.std)
-            to_mask = torch.zeros(sample.shape, device=samples.device).flatten(0 if self.feature_level == "channel" else 1)
-            if self.feature_level == "channel":
-                to_mask[indices[i]] = 1.
-            else:
-                to_mask[:, indices[i]] = 1.
-            to_mask = to_mask.reshape(sample.shape)
-            res.append(sample - (to_mask * sample) + (to_mask * baseline))
-        return torch.stack(res, dim=0)
+        batch_size, num_channels, rows, cols = samples.shape
+        num_indices = indices.shape[1]
+        batch_dim = np.tile(range(batch_size), (num_indices, 1)).transpose()
+
+        mean = samples if self.additive else torch.zeros(samples.shape, device=samples.device)
+        baseline = torch.normal(mean=mean, std=self.std)
+
+        to_mask = torch.zeros(samples.shape, device=samples.device).flatten(1 if self.feature_level == "channel" else 2)
+        if self.feature_level == "channel":
+            to_mask[batch_dim, indices] = 1.
+        else:
+            to_mask[batch_dim, :, indices] = 1.
+        to_mask = to_mask.reshape(samples.shape)
+        return samples - (to_mask * samples) + (to_mask * baseline)
 
     def predict_masked(self, samples, indices, model, return_masked_samples=False):
         preds = []
