3f610bed55eb26571a43af7f77dc2d0437092368
Arne Gevaert <arnegevaert@gmail.com>
2021-02-02 11:16:54 +0100

    global metric correlations + metric option in open dashboard

--- attrbench/suite/dashboard/components/pages/correlations_page.py
+++ attrbench/suite/dashboard/components/pages/correlations_page.py
@@ -31,6 +31,7 @@ class CorrelationsPage(Page):
 
             # Inter-metric correlation
             result.append(html.H2("Inter-metric correlations"))
+            """
             for method_name in self.result_obj.get_methods():
                 result.append(html.H3(method_name))
                 data = {metric_name: self.result_obj.data[metric_name][method_name].mean(axis=1)
@@ -38,7 +39,19 @@ class CorrelationsPage(Page):
                         if self.result_obj.data[metric_name][method_name].shape[0] > 1}
                 plot = CorrelationMap(pd.concat(data, axis=1), id=f"{method_name}-metric-corr")
                 result.append(plot.render())
+            """
+            data = {metric_name: [] for metric_name in self.result_obj.get_metrics()
+                    if self.result_obj.metadata[metric_name]["shape"][0] > 1}
+            for method_name in self.result_obj.get_methods():
+                for metric_name in self.result_obj.get_metrics():
+                    if self.result_obj.metadata[metric_name]["shape"][0] > 1:
+                        data[metric_name].append(self.result_obj.data[metric_name][method_name].mean(axis=1))
+            for metric_name in data.keys():
+                data[metric_name] = pd.concat(data[metric_name], axis=0)
+            plot = CorrelationMap(pd.concat(data, axis=1), id=f"metric-corr")
+            result.append(plot.render())
 
+            """
             # Inter-method correlation
             result.append(html.H2("Inter-method correlations"))
             metrics = [m for m in self.result_obj.get_metrics()
@@ -49,5 +62,6 @@ class CorrelationsPage(Page):
                         for method_name in self.result_obj.get_methods()}
                 plot = CorrelationMap(pd.concat(data, axis=1), id=f"{metric_name}-method-corr")
                 result.append(plot.render())
+            """
             self.rendered_contents = html.Div(result)
         return self.rendered_contents
