7906127b21d8ee0861f380bfec0a65e097cf79e7
Arne Gevaert <arnegevaert@gmail.com>
2021-02-24 14:50:39 +0100

    using datasets/dataloaders improves parallelism

--- attrbench/functional/infidelity.py
+++ attrbench/functional/infidelity.py
@@ -3,28 +3,33 @@ from typing import Callable
 import random
 from skimage.segmentation import slic
 import numpy as np
+from torch.utils.data import Dataset, DataLoader
 
 
-class Perturbation:
-    def __init__(self, samples, perturbation_size):
+class PerturbationDataset(Dataset):
+    def __init__(self, samples: np.ndarray, perturbation_size, num_perturbations):
         self.samples = samples
         self.perturbation_size = perturbation_size
+        self.num_perturbations = num_perturbations
 
-    def __call__(self):
+    def __len__(self):
+        return self.num_perturbations
+
+    def __getitem__(self, item):
         raise NotImplementedError
 
 
-class GaussianPerturbation(Perturbation):
+class GaussianPerturbation(PerturbationDataset):
     # perturbation_size is stdev of noise
-    def __call__(self):
+    def __getitem__(self, item):
         perturbation_vector = np.random.normal(0, self.perturbation_size, self.samples.shape)
         perturbed_samples = self.samples - perturbation_vector
         return perturbed_samples, perturbation_vector
 
 
-class SquareRemovalPerturbation(Perturbation):
+class SquareRemovalPerturbation(PerturbationDataset):
     # perturbation_size is (square height)/(image height)
-    def __call__(self):
+    def __getitem__(self, item):
         height = self.samples.shape[2]
         width = self.samples.shape[3]
         square_size_int = int(self.perturbation_size * height)
@@ -37,16 +42,16 @@ class SquareRemovalPerturbation(Perturbation):
         return perturbed_samples, perturbation_vector
 
 
-class SegmentRemovalPerturbation(Perturbation):
+class SegmentRemovalPerturbation(PerturbationDataset):
     # perturbation size is number of segments
-    def __init__(self, samples, perturbation_size):
-        super().__init__(samples, perturbation_size)
+    def __init__(self, samples, perturbation_size, num_perturbations):
+        super().__init__(samples, perturbation_size, num_perturbations)
         seg_samples = np.stack([slic(np.transpose(samples[i, ...], (1, 2, 0)),
                                      start_label=0, slic_zero=True)
                                for i in range(samples.shape[0])])
         self.seg_samples = np.expand_dims(seg_samples, axis=1)
 
-    def __call__(self):
+    def __getitem__(self, item):
         perturbed_samples, perturbation_vectors = [], []
         # This needs to happen per sample, since samples don't necessarily have
         # the same number of segments
@@ -61,7 +66,7 @@ class SegmentRemovalPerturbation(Perturbation):
             to_remove = np.isin(seg_sample, segments_to_mask)
             # Create perturbation vector by multiplying mask with image
             perturbation_vector = sample * to_remove.astype(np.float)
-            perturbed_samples.append(sample - perturbation_vector)
+            perturbed_samples.append((sample - perturbation_vector).astype(np.float))
             perturbation_vectors.append(perturbation_vector)
         return np.stack(perturbed_samples, axis=0), np.stack(perturbation_vectors, axis=0)
 
@@ -76,10 +81,13 @@ _PERTURBATION_CLASSES = {
 def infidelity(samples: torch.Tensor, labels: torch.Tensor, model: Callable, attrs: torch.Tensor,
                perturbation_mode: str, perturbation_size: float, num_perturbations: int,
                writer=None):
+    device = samples.device
     if perturbation_mode not in _PERTURBATION_CLASSES.keys():
         raise ValueError(f"Invalid perturbation mode {perturbation_mode}. "
                          f"Valid options are {', '.join(list(_PERTURBATION_CLASSES.keys()))}")
-    perturbation_fn = _PERTURBATION_CLASSES[perturbation_mode](samples.cpu().numpy(), perturbation_size)
+    perturbation_ds = _PERTURBATION_CLASSES[perturbation_mode](samples.cpu().numpy(),
+                                                               perturbation_size, num_perturbations)
+    perturbation_dl = DataLoader(perturbation_ds, batch_size=1, num_workers=4)
 
     # Get original model output
     with torch.no_grad():
@@ -93,11 +101,13 @@ def infidelity(samples: torch.Tensor, labels: torch.Tensor, model: Callable, att
     attrs_flattened = attrs.flatten(1)
 
     infid = []
-    for i_pert in range(num_perturbations):
+    for i_pert, (perturbed_samples, perturbation_vector) in enumerate(perturbation_dl):
+        print(i_pert)
         # Get perturbation vector I and perturbed samples (x - I)
-        perturbed_samples, perturbation_vector = perturbation_fn()
-        perturbed_samples = torch.tensor(perturbed_samples, dtype=torch.float).to(samples.device)
-        perturbation_vector = torch.tensor(perturbation_vector, dtype=torch.float).to(samples.device)
+        #perturbed_samples = torch.tensor(perturbed_samples[0], dtype=torch.float, device=device)
+        #perturbation_vector = torch.tensor(perturbation_vector[0], dtype=torch.float, device=device)
+        perturbed_samples = perturbed_samples[0].float().to(device)
+        perturbation_vector = perturbation_vector[0].float().to(device)
         if writer:
             writer.add_images("perturbation_vector", perturbation_vector, global_step=i_pert)
             writer.add_images("perturbed_samples", perturbed_samples, global_step=i_pert)
