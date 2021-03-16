ce6876ef5c6f78bb0da1aeaab723471836dbb58a
AxelJanRousseau <axeljanrousseau@gmail.com>
2021-03-16 15:50:24 +0100

    Initial branch commit
    add extra functions to masker

--- attrbench/lib/masking/masker.py
+++ attrbench/lib/masking/masker.py
@@ -9,8 +9,51 @@ class Masker:
             raise ValueError(f"feature_level must be 'channel' or 'pixel'. Found {feature_level}.")
         self.feature_level = feature_level
         self.baseline = None
+        self.samples = None
+        self.attributions = None
+        self.segments = None
 
-    def check_attribution_shape(self, samples, attributions: np.ndarray):
+    def initialize_samples(self, samples, attributions, segmentation = None):
+        if not self.check_attribution_shape(samples,attributions):
+            raise ValueError("Attributions are not compatible with masker")
+        self.initialize_baselines(samples)
+        self.samples=samples
+        self.attributions=attributions
+        self.segments=segmentation
+        self.sorted_indices = attributions.reshape(attributions.shape[0], -1).argsort()
+
+    # TODO: deal with segments: see irof
+    def mask_top(self, k, segmented=False):
+        if k==0:
+            return self.samples
+        return self.mask(self.samples, self.sorted_indices[:,-k:])
+    def mask_bot(self, k, segmented=False):
+        return self.mask(self.samples, self.sorted_indices[:,:k])
+    def keep_top(self, k, segmented=False):
+        if k==0:
+            return self.samples
+        return self.mask(self.samples, self.sorted_indices[:, :-k])
+    def keep_bot(self,k, segmented=False):
+        return self.mask(self.samples, self.sorted_indices[:,k:])
+    def mask_rand(self,k,segmented=False):
+        if k==0:
+            return self.samples
+        indices = np.arange(self.sorted_indices.shape(-1))
+        indices = np.tile(indices, (self.sorted_indices.shape[0],1))
+        rng=np.random.default_rng()
+        rng.shuffle(indices,axis=1)
+        return self.mask(self.samples, indices[:,:k])
+    def mask_all(self):
+        return self.mask(self.samples, self.sorted_indices)
+    def predict_masked(self, samples, indices, model, return_masked_samples=False):
+        masked = self.mask(samples, indices)
+        with torch.no_grad():
+            pred = model(masked)
+        if return_masked_samples:
+            return pred, masked
+        return pred
+
+    def check_attribution_shape(self, samples, attributions):
         if self.feature_level == "channel":
             # Attributions should be same shape as samples
             return list(samples.shape) == list(attributions.shape)
@@ -48,6 +91,8 @@ class Masker:
     def mask_boolean(self, samples, bool_mask):
         bool_mask = bool_mask
         return samples - (bool_mask * samples) + (bool_mask * self.baseline)
+    # why not return samples[bool_mask] = self.baseline[bool_mask] ?
+
 
     def initialize_baselines(self, samples):
         raise NotImplementedError
