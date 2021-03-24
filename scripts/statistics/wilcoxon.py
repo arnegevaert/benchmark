f64d1615c018e8f17bcf015e5a555a9269812e2b
Arne Gevaert <arnegevaert@gmail.com>
2021-03-24 16:14:08 +0100

    generalizations

--- scripts/statistics/wilcoxon.py
+++ scripts/statistics/wilcoxon.py
@@ -16,16 +16,30 @@ def cohend(x, y, type="mean"):
         raise ValueError("Type must be mean or median")
 
 
-def wilcoxon_infidelity(variant, activation):
+def plot_wilcoxon_result(title, effect_sizes, pvalues, labels):
+    fig, axs = plt.subplots(ncols=2, gridspec_kw={'width_ratios': [4, 1]})
+
+    effect_sizes.plot.barh(figsize=(14, 6), title=f"{title} effect sizes", ax=axs[0])
+    axs[0].legend(labels, loc='upper center', bbox_to_anchor=(0.5, -.05), ncol=3, fancybox=True,
+                  shadow=True)
+    axs[1].pcolor(pvalues < 0.05, cmap="RdYlGn", edgecolor="black")
+    axs[1].set_title("p < 0.05")
+    axs[1].set_yticks([])
+    axs[1].set_xticks(np.arange(3) + 0.5)
+    axs[1].tick_params(axis="x", rotation=45)
+    axs[1].set_xticklabels(labels, ha="right")
+    plt.show()
+
+
+def wilcoxon_tests(title, names, variant, effect_size_measure, labels):
     # Infidelity
-    infidelity_names = [f"infidelity_{pert}" for pert in ("gaussian", "seg", "sq")]
     all_pvalues, all_effect_sizes = {}, {}
-    for name in infidelity_names:
+    for name in names:
         pvalues, effect_sizes = {}, {}
         # Load dataframes from object
         df_dict = res_obj.metric_results[name].to_df()
         # We start by just looking at basic MSE-linear
-        df = df_dict[f"{variant}_{activation}"]
+        df = df_dict[f"{variant}"]
         # Take the first n rows for pilot study
         df = df.iloc[:PILOT_ROWS, :]
         # Remove the methods that we want to ignore
@@ -36,9 +50,9 @@ def wilcoxon_infidelity(variant, activation):
             statistic, pvalue = stats.wilcoxon(method_results, baseline_results, alternative="less")
             pvalues[method_name] = pvalue
             # Probability of superiority effect size
-            if variant == "mse":
+            if effect_size_measure == "cohend":
                 effect_sizes[method_name] = cohend(method_results, baseline_results)
-            elif variant == "corr":
+            elif effect_size_measure == "meandiff":
                 effect_sizes[method_name] = np.mean(method_results) - np.mean(baseline_results)
             else:
                 raise ValueError("Variant must be mse or corr")
@@ -46,27 +60,15 @@ def wilcoxon_infidelity(variant, activation):
         all_effect_sizes[name] = effect_sizes
     all_pvalues = pd.DataFrame.from_dict(all_pvalues)
     all_effect_sizes = pd.DataFrame.from_dict(all_effect_sizes)
-
-    fig, axs = plt.subplots(ncols=2, gridspec_kw={'width_ratios': [4, 1]})
-
-    all_effect_sizes.plot.barh(figsize=(14, 6), title="Infidelity effect sizes", ax=axs[0])
-    axs[0].legend(("Gaussian", "Segment", "Square"), loc='upper center', bbox_to_anchor=(0.5, -.05), ncol=3, fancybox=True,
-               shadow=True)
-    axs[1].pcolor(all_pvalues < 0.05, cmap="RdYlGn", edgecolor="black")
-    axs[1].set_title("p < 0.05")
-    axs[1].set_yticks([])
-    axs[1].set_xticks(np.arange(3) + 0.5)
-    axs[1].tick_params(axis="x", rotation=45)
-    axs[1].set_xticklabels(["Gaussian", "Segment", "Square"], ha="right")
-    plt.show()
+    plot_wilcoxon_result(title, all_effect_sizes, all_pvalues, labels)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("file", type=str)
     parser.add_argument("metric", type=str)
-    parser.add_argument("-v", "--variant", type=str, default="mse")
-    parser.add_argument("-a", "--activation", type=str, default="linear")
+    parser.add_argument("-v", "--variant", type=str, default="mse_linear")
+    parser.add_argument("-e", "--effect-size-measure", type=str, default="cohend", choices=["cohend", "meandiff"])
     args = parser.parse_args()
 
     IGNORE_METHODS = ["Random_pos_only", "GradCAM_no_relu", "GuidedGradCAM_no_relu"]
@@ -76,4 +78,8 @@ if __name__ == "__main__":
     res_obj = SuiteResult.load_hdf(args.file)
 
     if args.metric == "infidelity":
-        wilcoxon_infidelity(args.variant, args.activation)
+        infidelity_names = [f"infidelity_{pert}" for pert in ("gaussian", "seg", "sq")]
+        wilcoxon_tests("Infidelity", infidelity_names, args.variant, args.effect_size_measure, ("Gaussian", "Segment", "Square"))
+    elif args.metric == "deletion":
+        deletion_names = [f"masker_constant.deletion", "masker_blur.deletion", "masker_random.deletion"]
+        wilcoxon_tests("Deletion", deletion_names, args.variant, args.effect_size_measure, ("Constant", "Blur", "Random"))
