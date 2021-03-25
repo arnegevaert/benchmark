d08b53afb3dc910019a54da5354d84d80167b51e
Arne Gevaert <arnegevaert@gmail.com>
2021-03-25 15:43:37 +0100

    merge wilcoxon and power

--- scripts/statistics/util.py
+++ scripts/statistics/util.py
@@ -1,36 +1,56 @@
 import numpy as np
-from matplotlib import pyplot as plt
-import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
 
 
-def get_df(res, name, variant=None, activation=None, ignore_methods=None, log=False):
-    # Get variant/activation
-    if variant is None and activation is None:
-        df = res.metric_results[name].to_df()
-    elif variant is not None and activation is not None:
-        df = res.metric_results[name].to_df()[f"{variant}_{activation}"]
-    elif variant is not None:
-        df = res.metric_results[name].to_df()[f"{variant}"]
-    elif activation is not None:
-        df = res.metric_results[name].to_df()[f"{activation}"]
-    
-    # Ignore columns
-    df = df[df.columns.difference(ignore_methods)]
-    
-    # Log-transform
-    if log:
-        df = np.log(df)
-    return df
+def get_dfs(res_obj, metric_name, baseline_method, mode=None, activation=None, ignore_methods=None):
+    df_dict = res_obj.metric_results[metric_name].to_df()
+    df = df_dict[f"{mode}_{activation}"]
+    inverted = res_obj.metric_results[metric_name].inverted[mode]
+    if ignore_methods is not None:
+        df = df[df.columns.difference(ignore_methods)]
+    baseline = df[baseline_method]
+    df = df[df.columns.difference([baseline_method])]
+    return df, baseline, inverted
 
 
-def boxplot_grid(res, names, variant, activations, ignore_methods=None, log=False):
-    dfs = []
-    for activation in activations:
-        for name in names:
-            dfs.append((f"{name} - {activation}", get_df(res, name, variant, activation, ignore_methods, log)))
+def cohend(x, y, type="mean"):
+    pooled_std = np.sqrt(((x.shape[0] - 1) * np.var(x) + (y.shape[0] - 1) * np.var(y)) / (x.shape[0] + y.shape[0] - 2))
+    if type == "mean":
+        return (np.mean(x) - np.mean(y)) / pooled_std
+    elif type == "median":
+        return (np.median(x) - np.median(y)) / pooled_std
+    else:
+        raise ValueError("Type must be mean or median")
 
-    fig, axs = plt.subplots(nrows=len(activations), ncols=len(names), figsize=(10*len(names), 10*len(activations)))
-    axs = axs.flatten()
-    for i, (title, df) in enumerate(dfs):
-        sns.boxplot(ax=axs[i], data=df, orient="h")
-        axs[i].set_title(title)
+
+def plot_wilcoxon_result(title, effect_sizes, pvalues, labels):
+    fig, axs = plt.subplots(ncols=2, gridspec_kw={'width_ratios': [4, 1]})
+
+    effect_sizes.plot.barh(figsize=(14, 6), title=f"{title} effect sizes", ax=axs[0])
+    axs[0].legend(labels, loc='upper center', bbox_to_anchor=(0.5, -.05), ncol=3, fancybox=True,
+                  shadow=True)
+    axs[1].pcolor(pvalues < 0.05, cmap="RdYlGn", edgecolor="black")
+    axs[1].set_title("p < 0.05")
+    axs[1].set_yticks([])
+    axs[1].set_xticks(np.arange(3) + 0.5)
+    axs[1].tick_params(axis="x", rotation=45)
+    axs[1].set_xticklabels(labels, ha="right")
+    plt.show()
+
+
+def emp_power_curve(sample, baseline_sample, effect_size, iterations, n_range, inverted, tolerance, alpha):
+    emp_power = []
+    for n in n_range:
+        bs_indices = np.random.choice(np.arange(sample.shape[0]), size=(iterations, n))
+        bs_sample = sample[bs_indices]
+        bs_baseline = baseline_sample[bs_indices]
+        pvalues = [stats.wilcoxon(bs_sample[i, ...], bs_baseline[i, ...], alternative="less" if inverted else "greater")[1]
+                   for i in range(iterations)]
+        effect_sizes = [cohend(bs_sample[i, ...], bs_baseline[i, ...]) for i in range(iterations)]
+        significant = np.array(pvalues) < alpha
+        effect = (np.array(effect_sizes) <= tolerance * effect_size) \
+            if inverted else (np.array(effect_sizes) >= tolerance * effect_size)
+        detected = significant & effect
+        emp_power.append(detected.sum() / iterations)
+    return emp_power
--- scripts/statistics/wilcoxon.py
+++ scripts/statistics/wilcoxon.py
@@ -1,88 +1,102 @@
 from attrbench.suite import SuiteResult
-from scipy import stats
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import argparse
+from scipy import stats
+import numpy as np
+from scripts.statistics.util import get_dfs, cohend, plot_wilcoxon_result, emp_power_curve
 
 
-def cohend(x, y, type="mean"):
-    pooled_std = np.sqrt(((x.shape[0] - 1) * np.var(x) + (y.shape[0] - 1) * np.var(y)) / (x.shape[0] + y.shape[0] - 2))
-    if type == "mean":
-        return (np.mean(x) - np.mean(y)) / pooled_std
-    elif type == "median":
-        return (np.median(x) - np.median(y)) / pooled_std
-    else:
-        raise ValueError("Type must be mean or median")
-
-
-def plot_wilcoxon_result(title, effect_sizes, pvalues, labels):
-    fig, axs = plt.subplots(ncols=2, gridspec_kw={'width_ratios': [4, 1]})
-
-    effect_sizes.plot.barh(figsize=(14, 6), title=f"{title} effect sizes", ax=axs[0])
-    axs[0].legend(labels, loc='upper center', bbox_to_anchor=(0.5, -.05), ncol=3, fancybox=True,
-                  shadow=True)
-    axs[1].pcolor(pvalues < 0.05, cmap="RdYlGn", edgecolor="black")
-    axs[1].set_title("p < 0.05")
-    axs[1].set_yticks([])
-    axs[1].set_xticks(np.arange(3) + 0.5)
-    axs[1].tick_params(axis="x", rotation=45)
-    axs[1].set_xticklabels(labels, ha="right")
-    plt.show()
-
-
-def wilcoxon_tests(res_obj, title, names, mode, activation, effect_size_measure, labels):
-    # Infidelity
-    all_pvalues, all_effect_sizes = {}, {}
-    for name in names:
-        pvalues, effect_sizes = {}, {}
-        # Load dataframes from object
-        df_dict = res_obj.metric_results[name].to_df()
-        # Select the correct variant of the metric
-        df = df_dict[f"{mode}_{activation}"]
-        inverted = res_obj.metric_results[name].inverted[mode]
-        # Take the first n rows for pilot study
-        df = df.iloc[:PILOT_ROWS, :]
-        # Remove the methods that we want to ignore
-        df = df[df.columns.difference(IGNORE_METHODS)]
-        baseline_results = df[BASELINE].to_numpy()
-        for method_name in df.columns.difference([BASELINE]):
-            method_results = df[method_name].to_numpy()
-            statistic, pvalue = stats.wilcoxon(method_results, baseline_results,
-                                               alternative="less" if inverted else "greater")
-            pvalues[method_name] = pvalue
-            # Probability of superiority effect size
-            if effect_size_measure == "cohend":
-                effect_sizes[method_name] = cohend(method_results, baseline_results)
-            elif effect_size_measure == "meandiff":
-                effect_sizes[method_name] = np.mean(method_results) - np.mean(baseline_results)
-            else:
-                raise ValueError("Variant must be mse or corr")
-        all_pvalues[name] = pvalues
-        all_effect_sizes[name] = effect_sizes
-    all_pvalues = pd.DataFrame.from_dict(all_pvalues)
-    all_effect_sizes = pd.DataFrame.from_dict(all_effect_sizes)
-    plot_wilcoxon_result(title, all_effect_sizes, all_pvalues, labels)
+def wilcoxon_tests(df, baseline, effect_size_measure):
+    pvalues, effect_sizes = {}, {}
+    for method_name in df:
+        method_results = df[method_name].to_numpy()
+        statistic, pvalue = stats.wilcoxon(method_results, baseline,
+                                           alternative="less" if inverted else "greater")
+        pvalues[method_name] = pvalue
+        if effect_size_measure == "cohend":
+            effect_sizes[method_name] = cohend(method_results, baseline)
+        elif effect_size_measure == "meandiff":
+            effect_sizes[method_name] = np.mean(method_results) - np.mean(baseline)
+        else:
+            raise ValueError("Variant must be mse or corr")
+    return effect_sizes, pvalues
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("file", type=str)
     parser.add_argument("metric", type=str)
-    parser.add_argument("-m", "--mode", type=str, default="mse")
-    parser.add_argument("-a", "--activation", type=str, default="linear")
+    parser.add_argument("-m", "--mode", type=str)
+    parser.add_argument("-a", "--activation", type=str)
     parser.add_argument("-e", "--effect-size-measure", type=str, default="cohend", choices=["cohend", "meandiff"])
     args = parser.parse_args()
 
+    # Constant parameters, might be moved to args if necessary
     IGNORE_METHODS = ["Random_pos_only", "GradCAM_no_relu", "GuidedGradCAM_no_relu"]
     BASELINE = "Random"
     PILOT_ROWS = 128
+    PWR_ITERATIONS = 100
+    PWR_ALPHA = 0.01
+    PWR_N_RANGE = np.arange(5, 128, 5)
+    PWR_TOLERANCE = 0.9
     plt.rcParams["figure.dpi"] = 140
     res_obj = SuiteResult.load_hdf(args.file)
 
-    if args.metric == "infidelity":
-        infidelity_names = [f"infidelity_{pert}" for pert in ("gaussian", "seg", "sq")]
-        wilcoxon_tests(res_obj, "Infidelity", infidelity_names, args.mode, args.activation, args.effect_size_measure, ("Gaussian", "Segment", "Square"))
-    elif args.metric == "deletion":
-        deletion_names = [f"masker_constant.deletion", "masker_blur.deletion", "masker_random.deletion"]
-        wilcoxon_tests(res_obj, "Deletion", deletion_names, args.mode, args.activation, args.effect_size_measure, ("Constant", "Blur", "Random"))
+    # Extract necessary metric parameters
+    param_dict = {
+        "infidelity": {
+            "metric_names": [f"infidelity_{pert}" for pert in ("gaussian", "seg", "sq")],
+            "labels": ("Gaussian", "Segment", "Square"),
+            "title": f"Infidelity - {args.mode} - {args.activation}"
+        },
+        "deletion": {
+            "metric_names": [f"masker_constant.deletion", "masker_blur.deletion", "masker_random.deletion"],
+            "labels": ("Constant", "Blur", "Random"),
+            "title": f"Deletion - {args.mode} - {args.activation}"
+        },
+        "insertion": {
+            "metric_names": [f"masker_constant.insertion", "masker_blur.insertion", "masker_random.insertion"],
+            "labels": ("Constant", "Blur", "Random"),
+            "title": f"Insertion - {args.mode} - {args.activation}"
+        },
+        "irof": {
+            "metric_names": [f"masker_constant.irof", "masker_blur.irof", "masker_random.irof"],
+            "labels": ("Constant", "Blur", "Random"),
+            "title": f"IROF - {args.mode} - {args.activation}"
+        },
+        "iiof": {
+            "metric_names": [f"masker_constant.iiof", "masker_blur.iiof", "masker_random.iiof"],
+            "labels": ("Constant", "Blur", "Random"),
+            "title": f"IIOF - {args.mode} - {args.activation}"
+        }
+    }[args.metric]
+
+    effect_sizes, pvalues = {}, {}
+    for i, metric_name in enumerate(param_dict["metric_names"]):
+        # Compute effect sizes and p-values
+        df, baseline, inverted = get_dfs(res_obj, metric_name, BASELINE, args.mode,
+                                         args.activation, IGNORE_METHODS)
+        mes, mpv = wilcoxon_tests(df.iloc[:PILOT_ROWS, :], baseline[:PILOT_ROWS], args.effect_size_measure)
+        effect_sizes[metric_name] = mes
+        pvalues[metric_name] = mpv
+
+        # Compute power curves for significant methods
+        power_curves = {}
+        for method_name in mpv.keys():
+            if mpv[method_name] < PWR_ALPHA:
+                pilot_effect_size = cohend(df[method_name].iloc[:PILOT_ROWS], baseline.iloc[:PILOT_ROWS])
+                power_curves[method_name] = emp_power_curve(df[method_name].iloc[PILOT_ROWS:].to_numpy(),
+                                                            baseline.iloc[PILOT_ROWS:].to_numpy(),
+                                                            pilot_effect_size, PWR_ITERATIONS, PWR_N_RANGE, inverted,
+                                                            PWR_TOLERANCE,
+                                                            PWR_ALPHA)
+        for method_name, power_curve in power_curves.items():
+            plt.plot(PWR_N_RANGE, power_curve, label=method_name)
+        plt.legend()
+        plt.title(f"{param_dict['title']} - {param_dict['labels'][i]} - Power analysis")
+        plt.show()
+
+    effect_sizes = pd.DataFrame.from_dict(effect_sizes)
+    pvalues = pd.DataFrame.from_dict(pvalues)
+    plot_wilcoxon_result(param_dict["title"], effect_sizes, pvalues, param_dict["labels"])
