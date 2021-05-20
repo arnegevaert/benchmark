fixed IROF masking

attrbench/lib/segmentation.py
@@ -10,7 +10,7 @@ def isin(a: np.ndarray, b: np.ndarray):
     return (a[..., None] == b).any(-1)
 
 
-def mask_segments(images: np.ndarray, seg_images: np.ndarray, segments: List[List[int]], masker: Masker) -> torch.tensor:
+def mask_segments(images: torch.tensor, seg_images: np.ndarray, segments: List[List[int]], masker: Masker) -> torch.tensor:
     if not (images.shape[0] == seg_images.shape[0] and images.shape[0] == len(segments) and
             images.shape[-2:] == seg_images.shape[-2:]):
         raise ValueError(f"Incompatible shapes: {images.shape}, {seg_images.shape}, {len(segments)}")
@@ -20,7 +20,7 @@ def mask_segments(images: np.ndarray, seg_images: np.ndarray, segments: List[Lis
         segs = np.array(segments[i])
         bool_masks.append(isin(seg_img, segs))
     bool_masks = np.stack(bool_masks, axis=0)
-    return masker.mask_boolean(torch.tensor(images), torch.tensor(bool_masks))
+    return masker.mask_boolean(images, torch.tensor(bool_masks))
 
 
 def segment_samples(samples: np.ndarray) -> np.ndarray:
@@ -36,7 +36,7 @@ def segment_attributions(seg_images: np.ndarray, attrs: np.ndarray) -> np.ndarra
     segments = np.unique(seg_images)
     seg_img_flat = seg_images.reshape(seg_images.shape[0], -1)
     attrs_flat = attrs.reshape(attrs.shape[0], -1)
-    avg_attrs = np.zeros((seg_images.shape[0], np.max(segments)))
+    avg_attrs = np.zeros((seg_images.shape[0], np.max(segments) + 1))
     for seg in segments:
         mask = (seg_img_flat == seg).astype(int)
         masked_attrs = mask * attrs_flat

attrbench/metrics/insertion_deletion/_dataset.py
@@ -6,13 +6,28 @@ from attrbench.lib import mask_segments, segment_samples, segment_attributions
 from attrbench.lib.masking import Masker
 
 
-class _DeletionDataset:
-    def __init__(self, mode: str, start: float, stop: float, num_steps: int, samples: torch.tensor, attrs: np.ndarray,
-                 masker: Masker):
+class _MaskingDataset:
+    def __init__(self, mode: str, start: float, stop: float, num_steps: int):
         if mode not in ("morf", "lerf"):
             raise ValueError("Mode must be morf or lerf")
         if not ((0. <= start <= 1.) and (0. <= stop <= 1.)):
             raise ValueError("Start and stop must be between 0 and 1")
+        self.mode = mode
+        self.start = start
+        self.stop = stop
+        self.num_steps = num_steps
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, item):
+        raise NotImplementedError
+
+
+class _DeletionDataset(_MaskingDataset):
+    def __init__(self, mode: str, start: float, stop: float, num_steps: int, samples: torch.tensor, attrs: np.ndarray,
+                 masker: Masker):
+        super().__init__(mode, start, stop, num_steps)
         self.samples = samples
         self.masker = masker
         self.masker.initialize_baselines(samples)
@@ -38,13 +53,10 @@ class _DeletionDataset:
         return masked_samples
 
 
-class _IrofDataset:
+class _IrofDataset(_MaskingDataset):
     def __init__(self, mode: str, start: float, stop: float, num_steps: int, samples: torch.tensor, masker: Masker,
                  writer: AttributionWriter = None):
-        self.mode = mode
-        self.start = start
-        self.stop = stop
-        self.num_steps = num_steps
+        super().__init__(mode, start, stop, num_steps)
         self.samples = samples
         self.masker = masker
         self.masker.initialize_baselines(samples)
@@ -53,7 +65,7 @@ class _IrofDataset:
         # Override sorted_indices to use segment indices instead of pixel indices
         self.segmented_images = segment_samples(samples.cpu().numpy())
         if writer is not None:
-            writer.add_images("segmented samples", self.segmented_images)
+            writer.add_images("segmented samples", torch.tensor(self.segmented_images))
 
     def set_attrs(self, attrs: np.ndarray):
         avg_attrs = segment_attributions(self.segmented_images, attrs)
@@ -69,7 +81,7 @@ class _IrofDataset:
             sorted_indices.append(cur_sorted_indices if self.mode == "morf" else cur_sorted_indices[::-1])
             # Compute corresponding mask range for this image
             num_segments = len(cur_sorted_indices)
-            mask_ranges[i] = list((np.linspace(self.start, self.stop, self.num_steps) * num_segments).astype(np.int))
+            mask_ranges.append(list((np.linspace(self.start, self.stop, self.num_steps) * num_segments).astype(np.int)))
         self.sorted_indices = sorted_indices  # List[np.ndarray] (varying number of segments for each image)
         self.mask_ranges = mask_ranges
 
@@ -82,5 +94,8 @@ class _IrofDataset:
             # Get number of segments to mask for this image
             num_to_mask = self.mask_ranges[i][item]
             # Get num_segments most important (if morf) or least important (if lerf) segments
-            indices.append(self.sorted_indices[i][-num_to_mask:])
+            if num_to_mask == 0:
+                indices.append([])
+            else:
+                indices.append(self.sorted_indices[i][-num_to_mask:])
         return mask_segments(self.samples, self.segmented_images, indices, self.masker)

attrbench/metrics/insertion_deletion/_get_predictions.py
@@ -1,33 +1,22 @@
-from typing import Callable, Tuple, Dict
+from typing import Callable, List, Dict
 
 import torch
 
 from attrbench.lib.util import ACTIVATION_FNS
+from attrbench.metrics.insertion_deletion._dataset import _MaskingDataset
 
 
-def _get_predictions(samples: torch.Tensor, labels: torch.Tensor,
-                     model: Callable, ds,
-                     activation_fns: List[str], writer=None) -> Tuple[Dict, Dict, Dict]:
-    with torch.no_grad():
-        _orig_preds = model(samples)
-        orig_preds = {fn: ACTIVATION_FNS[fn](_orig_preds).gather(dim=1, index=labels.unsqueeze(-1)).cpu()
-                      for fn in activation_fns}
-        fully_masked = ds.masker.baseline
-        _neutral_preds = model(fully_masked)
-        neutral_preds = {fn: ACTIVATION_FNS[fn](_neutral_preds).gather(dim=1, index=labels.unsqueeze(-1)).cpu()
-                         for fn in activation_fns}
-        if writer is not None:
-            writer.add_images("orig_samples", samples)
-            writer.add_images("neutral_samples", fully_masked)
-
-    inter_preds = {fn: [] for fn in activation_fns}
-    for i in range(len(ds)):
-        batch = ds[i]
+def _get_predictions(masking_dataset: _MaskingDataset, labels: torch.Tensor,
+                     model: Callable,
+                     activation_fns: List[str], writer=None) -> Dict:
+    preds = {fn: [] for fn in activation_fns}
+    for i, batch in enumerate(masking_dataset):
         with torch.no_grad():
             predictions = model(batch)
         if writer is not None:
             writer.add_images("masked_samples", batch, global_step=i)
         for fn in activation_fns:
-            inter_preds[fn].append(ACTIVATION_FNS[fn](predictions).gather(dim=1, index=labels.unsqueeze(-1)).cpu())
-    return orig_preds, neutral_preds, inter_preds
-
+            preds[fn].append(ACTIVATION_FNS[fn](predictions).gather(dim=1, index=labels.unsqueeze(-1)).cpu())
+    for afn in activation_fns:
+        preds[afn] = torch.cat(preds[afn], dim=1).cpu()  # [batch_size, len(mask_range)]
+    return preds

attrbench/metrics/insertion_deletion/deletion.py
@@ -8,7 +8,7 @@ from attrbench.lib.masking import Masker
 from attrbench.metrics import MaskerMetric
 from ._dataset import _DeletionDataset
 from .result import DeletionResult
-from attrbench.lib.util import ACTIVATION_FNS
+from ._get_predictions import _get_predictions
 
 
 def deletion(samples: torch.Tensor, labels: torch.Tensor, model: Callable, attrs: np.ndarray,
@@ -19,21 +19,7 @@ def deletion(samples: torch.Tensor, labels: torch.Tensor, model: Callable, attrs
     if type(activation_fns) == str:
         activation_fns = [activation_fns]
     ds = _DeletionDataset(mode, start, stop, num_steps, samples, attrs, masker)
-
-    preds = {fn: [] for fn in activation_fns}
-    for i in range(len(ds)):
-        batch = ds[i]
-        with torch.no_grad():
-            predictions = model(batch)
-        if writer is not None:
-            writer.add_images("masked_samples", batch, global_step=i)
-        for fn in activation_fns:
-            preds[fn].append(ACTIVATION_FNS[fn](predictions).gather(dim=1, index=labels.unsqueeze(-1)).cpu())
-
-    for afn in activation_fns:
-        preds[afn] = torch.cat(preds[afn], dim=1).cpu()  # [batch_size, len(mask_range)]
-
-    return preds
+    return _get_predictions(ds, labels, model, activation_fns, writer)
 
 
 class Deletion(MaskerMetric):

attrbench/metrics/insertion_deletion/irof_iiof.py
@@ -7,7 +7,6 @@ from attrbench.lib.masking import Masker
 from attrbench.lib.attribution_writer import AttributionWriter
 from os import path
 from attrbench.metrics import MaskerMetric
-from ._concat_results import _concat_results
 from ._dataset import _IrofDataset
 from ._get_predictions import _get_predictions
 from .result import IrofResult
@@ -18,28 +17,10 @@ def irof(samples: torch.Tensor, labels: torch.Tensor, model: Callable, attrs: np
          mode: str = "morf", start: float = 0., stop: float = 1., num_steps: int = 100,
          writer=None):
     masking_dataset = _IrofDataset(mode, start, stop, num_steps, samples, masker, writer)
-    return _irof(samples, labels, model, attrs, masking_dataset, activation_fns, writer)
-
-
-def _irof(samples: torch.Tensor, labels: torch.Tensor, model: Callable, attrs: np.ndarray,
-          masking_dataset, activation_fn: Union[List[str], str] = "linear",
-          writer=None):
-    if type(activation_fn) == str:
-        activation_fn = [activation_fn]
+    if type(activation_fns) == str:
+        activation_fns = [activation_fns]
     masking_dataset.set_attrs(attrs)
-    orig_preds, neutral_preds, inter_preds = _get_predictions(samples, labels, model, masking_dataset, activation_fn,
-                                                              writer)
-    preds = _concat_results(orig_preds, inter_preds, neutral_preds, orig_preds)
-
-    # Calculate AUC for each sample (depends on how many segments each sample had)
-    result = {}
-    for fn in activation_fn:
-        auc = []
-        for i in range(samples.shape[0]):
-            num_segments = len(np.unique(masking_dataset.segmented_images[i, ...].cpu().numpy()))
-            auc.append(np.trapz(preds[fn][i, :num_segments + 1], x=np.linspace(0, 1, num_segments + 1)))
-        result[fn] = torch.tensor(auc).unsqueeze(-1)
-    return result
+    return _get_predictions(masking_dataset, labels, model, activation_fns, writer)
 
 
 class Irof(MaskerMetric):
@@ -64,18 +45,19 @@ class Irof(MaskerMetric):
         methods_result = {masker_name: {afn: {} for afn in self.activation_fns} for masker_name in self.maskers}
         baseline_result = {masker_name: {afn: [] for afn in self.activation_fns} for masker_name in self.maskers}
         for masker_name, masker in self.maskers.items():
-            masking_datasets[masker_name] = _IrofDataset(self.mode, samples, masker, self._get_writer(masker_name))
+            masking_datasets[masker_name] = _IrofDataset(self.mode, self.start, self.stop, self.num_steps,
+                                                         samples, masker, self._get_writer(masker_name))
         for masker_name, masking_dataset in masking_datasets.items():
             for method_name in attrs_dict:
-                result = _irof(samples, labels, self.model, attrs_dict[method_name],
-                               masking_dataset, self.activation_fns,
-                               writer=self._get_writer(method_name))
+                masking_dataset.set_attrs(attrs_dict[method_name])
+                result = _get_predictions(masking_dataset, labels, self.model, self.activation_fns,
+                                          self._get_writer(method_name))
                 for afn in self.activation_fns:
                     methods_result[masker_name][afn][method_name] = result[afn].cpu().detach().numpy()
 
             for i in range(baseline_attrs.shape[0]):
-                bl_result = _irof(samples, labels, self.model, baseline_attrs[i, ...],
-                                  masking_dataset, self.activation_fns)
+                masking_dataset.set_attrs(baseline_attrs[i, ...])
+                bl_result = _get_predictions(masking_dataset, labels, self.model, self.activation_fns)
                 for afn in self.activation_fns:
                     baseline_result[masker_name][afn].append(bl_result[afn].cpu().detach().numpy())
             for afn in self.activation_fns: