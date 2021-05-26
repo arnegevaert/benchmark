a04491e49224080452335a6b484a0b0ea2749134
Arne Gevaert <arnegevaert@gmail.com>
2021-05-26 10:45:44 +0200

    Remove infidelity loss functions

--- attrbench/metrics/infidelity/result.py
+++ attrbench/metrics/infidelity/result.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List
 import pandas as pd
 import numpy as np
 
@@ -9,19 +9,14 @@ from attrbench.lib import NDArrayTree
 
 
 class InfidelityResult(AbstractMetricResult):
-    inverted = {
-        "mse": True,
-        "normalized_mse": True,
-        "corr": False
-    }
+    inverted = True
 
     def __init__(self, method_names: List[str], perturbation_generators: List[str],
-                 activation_fns: List[str], loss_fns: List[str]):
+                 activation_fns: List[str]):
         super().__init__(method_names)
         self._tree = NDArrayTree([
             ("perturbation_generator", perturbation_generators),
             ("activation_fn", activation_fns),
-            ("loss_fn", loss_fns),
             ("method", method_names)
         ])
 
@@ -29,14 +24,13 @@ class InfidelityResult(AbstractMetricResult):
     def load_from_hdf(cls, group: h5py.Group):
         pert_gens = list(group.keys())
         activation_fns = list(group[pert_gens[0]].keys())
-        loss_fns = list(group[pert_gens[0]][activation_fns[0]].keys())
-        method_names = list(group[pert_gens[0]][activation_fns[0]][loss_fns[0]].keys())
-        result = cls(method_names, pert_gens, activation_fns, loss_fns)
-        result._tree = NDArrayTree.load_from_hdf(["perturbation_generator", "activation_fn", "loss_fn", "method"], group)
+        method_names = list(group[pert_gens[0]][activation_fns[0]].keys())
+        result = cls(method_names, pert_gens, activation_fns)
+        result._tree = NDArrayTree.load_from_hdf(["perturbation_generator", "activation_fn", "method"], group)
         return result
 
     def get_df(self, mode="raw", include_baseline=False, perturbation_generator="gaussian",
-               activation_fn="linear", loss_fn="mse", log=False):
+               activation_fn="linear", log=False):
         def _squeeze(x):
             return np.squeeze(x, axis=-1)
 
@@ -50,16 +44,14 @@ class InfidelityResult(AbstractMetricResult):
                 postproc_fn=postproc_fn,
                 exclude=dict(method=["_BASELINE"]),
                 select=dict(perturbation_generator=[perturbation_generator],
-                            activation_fn=[activation_fn],
-                            loss_fn=[loss_fn])
-            )[perturbation_generator][activation_fn][loss_fn]
+                            activation_fn=[activation_fn])
+            )[perturbation_generator][activation_fn]
         )
         baseline_results = pd.DataFrame(self.tree.get(
             postproc_fn=postproc_fn,
             select=dict(perturbation_generator=[perturbation_generator],
                         activation_fn=[activation_fn],
-                        loss_fn=[loss_fn],
                         method=["_BASELINE"])
-        )[perturbation_generator][activation_fn][loss_fn]["_BASELINE"])
+        )[perturbation_generator][activation_fn]["_BASELINE"])
         res, _ = self._get_df(raw_results, baseline_results, mode, include_baseline)
-        return res, self.inverted[loss_fn]
+        return res, self.inverted
