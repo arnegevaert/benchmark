e9e6fca77eab7e10581a95d96581b9e7bb7d8ee4
Arne Gevaert <arnegevaert@gmail.com>
2021-07-09 14:54:58 +0200

    simpler sandbox

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -1,4 +1,4 @@
-from experiments.lib.attribution import NewExpectedGradients, ExpectedGradients
+from experiments.lib.attribution import ExpectedGradients
 from experiments.general_imaging.lib.dataset_models import get_dataset_model
 from torch.utils.data import DataLoader
 from torchvision.utils import make_grid
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     model.eval()
     model.cuda()
     eg = ExpectedGradients(model, ds, num_samples=125)
-    new_eg = NewExpectedGradients(model, ds, num_samples=125)
 
     dl = DataLoader(ds, batch_size=1, shuffle=True)
     batch, labels = next(iter(dl))
@@ -25,15 +24,9 @@ if __name__ == "__main__":
     expl = torch.mean(expl, dim=1, keepdim=True).repeat(1, 3, 1, 1)
     end_t = time()
     print(f"{(end_t - start_t):.3f}")
-    print("Running new EG...")
-    start_t = time()
-    new_expl = new_eg(batch, labels).cpu()
-    new_expl = torch.mean(new_expl, dim=1, keepdim=True).repeat(1, 3, 1, 1)
-    end_t = time()
-    print(f"{(end_t - start_t):.3f}")
 
     imgs = (batch - batch.min()) / (batch.max() - batch.min())
-    all = torch.cat([expl, new_expl, imgs.cpu()])
+    all = torch.cat([expl, imgs.cpu()])
 
     plt.imshow(F.to_pil_image(make_grid(all)))
     plt.show()
