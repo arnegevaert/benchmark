571c8e9f612c1135de4d01bd298ee2eeb6be4619
Arne Gevaert <arnegevaert@gmail.com>
2021-08-31 14:45:13 +0200

    better krippendorff alpha

--- experiments/general_imaging/plot/wilcoxon.py
+++ experiments/general_imaging/plot/wilcoxon.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         else:
             dfs = get_default_dfs(res_obj, mode=args.mode)
         figsize = (10, 25) if args.all else (10, 10)
-        glyph_scale = 100 if args.all else 1000
+        glyph_scale = 1000
         fig = WilcoxonSummaryPlot(dfs).render(figsize=figsize, glyph_scale=glyph_scale)
         fig.savefig(path.join(args.out_dir, f"{ds_name}.png"), bbox_inches="tight")
         plt.close(fig)
