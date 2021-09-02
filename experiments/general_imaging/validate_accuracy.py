12745134ab61785eb6d9b4a3ffaca76d8610ac68
Arne Gevaert <arnegevaert@gmail.com>
2021-09-02 11:00:54 +0200

    dkqms

--- experiments/general_imaging/validate_accuracy.py
+++ experiments/general_imaging/validate_accuracy.py
@@ -23,19 +23,22 @@ def test_epoch(model, dl):
 
             index_order = torch.argsort(out, dim=1)  # Get the indices from low to high
             # If final index matches label, top-one is correct
-            top_one.extend((index_order[:, -1] == labels.view(-1, 1)).any(dim=1).cpu().numpy())
+            top_one.append((index_order[:, -1].view(-1, 1) == labels.view(-1, 1)).any(dim=1))
             # If final 5 indices contain label, top-five is correct
-            top_five.extend((index_order[:, -5:] == labels.view(-1, 1)).any(dim=1).cpu().numpy())
+            top_five.append((index_order[:, -5:] == labels.view(-1, 1)).any(dim=1))
 
             predictions.extend(torch.softmax(out, dim=1).cpu().numpy())
             true_labels.extend(labels.cpu().numpy())
         predictions = np.vstack(predictions)
 
-        top_one_acc = sum(top_one) / len(top_one)
-        top_five_acc = sum(top_five) / len(top_five)
+        top_one = torch.cat(top_one)
+        top_five = torch.cat(top_five)
+
+        top_one_acc = torch.sum(top_one) / top_one.shape[0]
+        top_five_acc = torch.sum(top_five) / top_five.shape[0]
         balanced_acc = metrics.balanced_accuracy_score(true_labels, predictions.argmax(axis=1))
         auc = metrics.roc_auc_score(true_labels, predictions, average="macro", multi_class='ovo', labels=np.arange(predictions.shape[1]))
-    return top_one_acc, top_five_acc, balanced_acc, auc
+    return top_one_acc.item(), top_five_acc.item(), balanced_acc, auc
 
 
 
@@ -54,7 +57,7 @@ if __name__ == "__main__":
     ds, model, sample_shape = get_dataset_model(args.dataset, args.model)
     model.eval()
     dl = DataLoader(ds, batch_size=args.batch_size, shuffle=False, num_workers=4)
-    top_one, top_five,balanced_acc, auc = test_epoch(model, dl)
+    top_one, top_five, balanced_acc, auc = test_epoch(model, dl)
     print("validation set results:\n"
           "top-one accuracy: {:f} \n"
           "top-five accuracy: {:f} \n"
