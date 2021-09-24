9c1bd2100c82ccdc896938ec686ddad47ac61455
Arne Gevaert <arnegevaert@gmail.com>
2021-09-24 11:15:59 +0200

    moved experiment code to new repo

--- experiments/medical_imaging/lib/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .alexnet import Alexnet
-from .densenet import Densenet
-from .resnet import Resnet
-from .medmnist_resnet import MedMnist_ResNet18
-from .efficientnet import EfficientNet
-from .senet import senet154
