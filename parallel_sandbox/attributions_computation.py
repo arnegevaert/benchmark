4c9ce6968ab1cfef1d5fbe0733e30a26d60368dc
Arne Gevaert <arne.gevaert@ugent.be>
2022-10-13 14:48:06 +0200

    add arguments to test scripts

--- parallel_sandbox/attributions_computation.py
+++ parallel_sandbox/attributions_computation.py
@@ -1,7 +1,8 @@
-from util.get_dataset_model import get_dataset, get_model
+from util.get_dataset_model import get_model
 from attrbench.distributed import AttributionsComputation
 from attrbench.data import HDF5Dataset, AttributionsDatasetWriter
 from captum import attr
+import argparse
 
 
 def method_factory(model):
@@ -11,13 +12,19 @@ def method_factory(model):
     return {
         "Gradient": saliency.attribute,
         "InputXGradient": ixg.attribute,
-        "IntegratedGradients": lambda x, y: ig.attribute(inputs=x, target=y, internal_batch_size=1)
+        "IntegratedGradients": lambda x, y: ig.attribute(inputs=x, target=y, internal_batch_size=args.batch_size)
     }
 
 
 if __name__ == "__main__":
-    dataset = HDF5Dataset("samples.h5")
-    writer = AttributionsDatasetWriter("attributions.h5", truncate=True, num_samples=len(dataset),
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset", type=str, default="samples.h5")
+    parser.add_argument("-b", "--batch-size", type=int, default=16)
+    parser.add_argument("-o", "--output-file", type=str, default="attributions.h5")
+    args = parser.parse_args()
+
+    dataset = HDF5Dataset(args.dataset)
+    writer = AttributionsDatasetWriter(args.output_file, truncate=True, num_samples=len(dataset),
                                        sample_shape=dataset.sample_shape)
-    computation = AttributionsComputation(get_model, method_factory, dataset, batch_size=16, writer=writer)
+    computation = AttributionsComputation(get_model, method_factory, dataset, batch_size=args.batch_size, writer=writer)
     computation.start()
--- parallel_sandbox/sample_selection.py
+++ parallel_sandbox/sample_selection.py
@@ -1,9 +1,16 @@
 from util.get_dataset_model import get_dataset, get_model
 from attrbench.data import HDF5DatasetWriter
 from attrbench.distributed import SampleSelection
+import argparse
+
 
 if __name__ == "__main__":
-    num_samples = 128
-    writer = HDF5DatasetWriter(path="samples.h5", num_samples=num_samples, sample_shape=(3, 224, 224))
-    sample_selection = SampleSelection(get_model, get_dataset(), writer, num_samples=num_samples, batch_size=16)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--num-samples", type=int, default=128)
+    parser.add_argument("-b", "--batch-size", type=int, default=16)
+    parser.add_argument("-o", "--output-file", type=str, default="samples.h5")
+    args = parser.parse_args()
+
+    writer = HDF5DatasetWriter(path="samples.h5", num_samples=args.num_samples, sample_shape=(3, 224, 224))
+    sample_selection = SampleSelection(get_model, get_dataset(), writer, num_samples=args.num_samples, batch_size=args.batch_size)
     sample_selection.start()
