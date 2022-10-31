a836f3a21a4df74e471058cb081023eeea066f03
Arne Gevaert <arne.gevaert@ugent.be>
2022-10-31 14:37:18 +0100

    basic script for getting results

---
+++ parallel_sandbox/get_deletion_result.py
@@ -0,0 +1,6 @@
+from attrbench.distributed.metrics.deletion import DeletionResult
+
+
+if __name__ == "__main__":
+    res = DeletionResult.load("deletion.h5")
+    df, higher_is_better = res.get_df("constant", "linear")
