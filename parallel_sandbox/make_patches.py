c6ea7b9e4ce2f14bd0b99f56a869989cca587285
Arne Gevaert <arne.gevaert@ugent.be>
2023-01-04 09:42:37 +0100

    fix sandbox script

--- parallel_sandbox/make_patches.py
+++ parallel_sandbox/make_patches.py
@@ -5,11 +5,11 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--num-patches", type=int, default=10)
+    parser.add_argument("-n", "--num-labels", type=int, default=10)
     parser.add_argument("-b", "--batch-size", type=int, default=32)
     parser.add_argument("-o", "--output-dir", type=str, default="patches")
     args = parser.parse_args()
 
-    make_patches = MakePatches(get_model, get_dataset(), num_patches=args.num_patches,
-                               batch_size=args.batch_size, target_label=0, path=args.output_dir)
+    make_patches = MakePatches(get_model, get_dataset(), num_labels=args.num_labels,
+                               batch_size=args.batch_size, path=args.output_dir)
     make_patches.run()
