fix synchronized execution sample selection and attributions computation

attrbench/distributed/attributions_computation.py
@@ -1,4 +1,5 @@
-from attrbench.distributed import PartialResultMessage, DoneMessage,  DistributedComputation, DistributedSampler, Worker
+from attrbench.distributed import PartialResultMessage, DoneMessage,\
+    DistributedComputation, DistributedSampler, Worker
 from attrbench import AttributionMethod
 from attrbench.data import AttributionsDatasetWriter, IndexDataset
 import torch.multiprocessing as mp
@@ -11,26 +12,33 @@ from tqdm import tqdm
 
 
 class AttributionResult:
-    def __init__(self, indices: npt.NDArray, attributions: npt.NDArray, method_name: str):
+    def __init__(self, indices: npt.NDArray, attributions: npt.NDArray,
+                 method_name: str):
         self.indices = indices
         self.attributions = attributions
         self.method_name = method_name
 
 
 class AttributionsWorker(Worker):
-    def __init__(self, result_queue: mp.Queue, rank: int, world_size: int, all_processes_done: mp.Event,
+    def __init__(self, result_queue: mp.Queue, rank: int, world_size: int,
+                 all_processes_done: mp.Event,
+                 distributed_computation: DistributedComputation,
                  model_factory: Callable[[], nn.Module],
-                 method_factory: Callable[[nn.Module], Dict[str, AttributionMethod]],
+                 method_factory: Callable[
+                     [nn.Module], Dict[str, AttributionMethod]],
                  dataset: IndexDataset, batch_size: int):
-        super().__init__(result_queue, rank, world_size, all_processes_done)
+        super().__init__(result_queue, rank, world_size, all_processes_done,
+                         distributed_computation)
         self.batch_size = batch_size
         self.dataset = dataset
         self.method_factory = method_factory
         self.model_factory = model_factory
 
     def work(self):
-        sampler = DistributedSampler(self.dataset, self.world_size, self.rank, shuffle=False)
-        dataloader = DataLoader(self.dataset, sampler=sampler, batch_size=self.batch_size, num_workers=4)
+        sampler = DistributedSampler(self.dataset, self.world_size, self.rank,
+                                     shuffle=False)
+        dataloader = DataLoader(self.dataset, sampler=sampler,
+                                batch_size=self.batch_size, num_workers=4)
         device = torch.device(self.rank)
         model = self.model_factory()
         model.to(device)
@@ -42,16 +50,18 @@ class AttributionsWorker(Worker):
             for method_name, method in method_dict.items():
                 with torch.no_grad():
                     attrs = method(batch_x, batch_y)
-                    result = AttributionResult(batch_indices.cpu().numpy(), attrs.cpu().numpy(), method_name)
-                    self.result_queue.put(PartialResultMessage(self.rank, result))
-        self.result_queue.put(DoneMessage(self.rank))
+                    result = AttributionResult(batch_indices.cpu().numpy(),
+                                               attrs.cpu().numpy(), method_name)
+                    self.send_result(PartialResultMessage(self.rank, result))
 
 
 class AttributionsComputation(DistributedComputation):
     def __init__(self, model_factory: Callable[[], nn.Module],
-                 method_factory: Callable[[nn.Module], Dict[str, AttributionMethod]],
+                 method_factory: Callable[[nn.Module],
+                                          Dict[str, AttributionMethod]],
                  dataset: Dataset, batch_size: int,
-                 writer: AttributionsDatasetWriter, address="localhost", port="12355", devices: Tuple = None):
+                 writer: AttributionsDatasetWriter, address="localhost",
+                 port="12355", devices: Tuple = None):
         super().__init__(address, port, devices)
         self.model_factory = model_factory
         self.method_factory = method_factory
@@ -64,11 +74,15 @@ class AttributionsComputation(DistributedComputation):
         self.prog = tqdm()
         super().run()
 
-    def _create_worker(self, queue: mp.Queue, rank: int, all_processes_done: mp.Event) -> Worker:
-        return AttributionsWorker(queue, rank, self.world_size, all_processes_done, self.model_factory,
-                                  self.method_factory, self.dataset, self.batch_size)
+    def _create_worker(self, queue: mp.Queue, rank: int,
+                       all_processes_done: mp.Event) -> Worker:
+        return AttributionsWorker(queue, rank, self.world_size,
+                                  all_processes_done, self, self.model_factory,
+                                  self.method_factory, self.dataset,
+                                  self.batch_size)
 
-    def _handle_result(self, result_message: PartialResultMessage[AttributionResult]):
+    def _handle_result(
+            self, result_message: PartialResultMessage[AttributionResult]):
         indices = result_message.data.indices
         attributions = result_message.data.attributions
         method_name = result_message.data.method_name

attrbench/distributed/distributed_computation.py
@@ -1,26 +1,26 @@
 from attrbench.distributed import PartialResultMessage, DoneMessage, Worker
 import torch.multiprocessing as mp
 import queue as q
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 import torch
 import os
 
 
 class DistributedComputation:
-    def __init__(self, address="localhost", port="12355", devices: Optional[Tuple[int]] = None):
+    def __init__(self, address="localhost", port="12355", 
+                 devices: Optional[Tuple[int]] = None):
         self.address = address
         self.port = port
-        self.devices = devices if devices is not None else list(range(torch.cuda.device_count()))
+        self.devices = devices if devices is not None \
+            else list(range(torch.cuda.device_count()))
         self.world_size = len(self.devices)
         self.ctx = mp.get_context("spawn")
 
     def _handle_result(self, result: PartialResultMessage):
         raise NotImplementedError
-    
-    def _finish(self):
-        raise NotImplementedError
 
-    def _create_worker(self, queue: mp.Queue, rank: int, all_processes_done: mp.Event) -> Worker:
+    def _create_worker(self, queue: mp.Queue, rank: int,
+                       all_processes_done: mp.Event) -> Worker:
         raise NotImplementedError
 
     def run(self):
@@ -31,7 +31,8 @@ class DistributedComputation:
             ctx = mp.get_context("spawn")
 
             queue = ctx.Queue()  # Will store results from metric
-            all_processes_done = ctx.Event()  # Used for signaling processes that they can terminate
+            # Used for signaling processes that they can terminate
+            all_processes_done = ctx.Event()
             processes = []  # Used for joining all processes
 
             # Start all processes
@@ -42,13 +43,14 @@ class DistributedComputation:
                 processes.append(p)
 
             # Gather results
-            # This is not busy waiting: queue.get will block until a result is passed
+            # This is not busy waiting: 
+            # queue.get will block until a result is passed
             done_processes = [False for _ in processes]
             while not all(done_processes):
-                res = queue.get()  # res is a PartialResultMessage or a DoneMessage
+                # res is a PartialResultMessage or a DoneMessage
+                res = queue.get()
                 if isinstance(res, DoneMessage):
                     done_processes[res.rank] = True
-                    self._finish()
                 else:
                     self._handle_result(res)
 

attrbench/distributed/sample_selection.py
@@ -1,4 +1,5 @@
-from attrbench.distributed import DistributedSampler, DistributedComputation, PartialResultMessage, DoneMessage, Worker
+from attrbench.distributed import DistributedSampler, DistributedComputation,\
+    PartialResultMessage, DoneMessage, Worker
 from tqdm import tqdm
 from attrbench.data import HDF5DatasetWriter
 from typing import Callable, Tuple
@@ -16,10 +17,14 @@ class SamplesResult:
 
 
 class SampleSelectionWorker(Worker):
-    def __init__(self, result_queue: mp.Queue, rank: int, world_size: int, all_processes_done: mp.Event,
-                 sufficient_samples: mp.Event, batch_size: int, dataset: Dataset,
+    def __init__(self, result_queue: mp.Queue, rank: int, world_size: int,
+                 all_processes_done: mp.Event, 
+                 distributed_computation: DistributedComputation,
+                 sufficient_samples: mp.Event, batch_size: int,
+                 dataset: Dataset,
                  model_factory: Callable[[], nn.Module]):
-        super().__init__(result_queue, rank, world_size, all_processes_done)
+        super().__init__(result_queue, rank, world_size, all_processes_done,
+                         distributed_computation)
         self.sufficient_samples = sufficient_samples
         self.model_factory = model_factory
         self.dataset = dataset
@@ -27,7 +32,8 @@ class SampleSelectionWorker(Worker):
 
     def work(self):
         sampler = DistributedSampler(self.dataset, self.world_size, self.rank)
-        dataloader = DataLoader(self.dataset, sampler=sampler, batch_size=self.batch_size,
+        dataloader = DataLoader(self.dataset, sampler=sampler,
+                                batch_size=self.batch_size,
                                 num_workers=4, pin_memory=True)
         device = torch.device(self.rank)
         model = self.model_factory()
@@ -41,15 +47,16 @@ class SampleSelectionWorker(Worker):
                 output = torch.argmax(model(batch_x), dim=1)
             correct_samples = batch_x[output == batch_y, ...]
             correct_labels = batch_y[output == batch_y]
-            result = SamplesResult(correct_samples.cpu().numpy(), correct_labels.cpu().numpy())
-            self.result_queue.put(PartialResultMessage(self.rank, result))
+            result = SamplesResult(correct_samples.cpu().numpy(),
+                                   correct_labels.cpu().numpy())
+            self.send_result(PartialResultMessage(self.rank, result))
             if self.sufficient_samples.is_set():
                 break
-        self.result_queue.put(DoneMessage(self.rank))
 
 
 class SampleSelection(DistributedComputation):
-    def __init__(self, model_factory: Callable[[], nn.Module], dataset: Dataset, writer: HDF5DatasetWriter,
+    def __init__(self, model_factory: Callable[[], nn.Module], dataset: Dataset,
+                 writer: HDF5DatasetWriter,
                  num_samples: int, batch_size: int,
                  address: str = "localhost",
                  port: str = "12355", devices: Tuple = None):
@@ -63,9 +70,13 @@ class SampleSelection(DistributedComputation):
         self.count = 0
         self.prog = None
 
-    def _create_worker(self, queue: mp.Queue, rank: int, all_processes_done: mp.Event):
-        return SampleSelectionWorker(queue, rank, self.world_size, all_processes_done, self.sufficient_samples,
-                                     self.batch_size, self.dataset, self.model_factory)
+    def _create_worker(self, queue: mp.Queue, rank: int,
+                       all_processes_done: mp.Event):
+        return SampleSelectionWorker(queue, rank, self.world_size,
+                                     all_processes_done, self,
+                                     self.sufficient_samples,
+                                     self.batch_size, self.dataset,
+                                     self.model_factory)
 
     def run(self):
         self.prog = tqdm(total=self.num_samples)

attrbench/metrics/distributed_metric.py
@@ -10,8 +10,10 @@ from typing import Tuple, Callable, Optional
 
 
 class DistributedMetric(DistributedComputation):
-    def __init__(self, model_factory: Callable[[], nn.Module], dataset: IndexDataset, batch_size: int,
-                 address="localhost", port="12355", devices: Optional[Tuple] = None):
+    def __init__(self, model_factory: Callable[[], nn.Module],
+                 dataset: IndexDataset, batch_size: int,
+                 address="localhost", port="12355",
+                 devices: Optional[Tuple] = None):
         super().__init__(address, port, devices)
         self.batch_size = batch_size
         self.dataset = dataset
@@ -19,7 +21,8 @@ class DistributedMetric(DistributedComputation):
         self.prog = None  # TQDM progress bar
         self._result: Optional[MetricResult] = None
 
-    def _create_worker(self, queue: mp.Queue, rank: int, all_processes_done: mp.Event) -> MetricWorker:
+    def _create_worker(self, queue: mp.Queue, rank: int,
+                       all_processes_done: mp.Event) -> MetricWorker:
         raise NotImplementedError
 
     def run(self, result_path: Optional[str] = None, progress=True):
@@ -40,7 +43,3 @@ class DistributedMetric(DistributedComputation):
             self._result.add(result_message.data)
         if self.prog is not None:
             self.prog.update(len(result_message.data.indices))
-    
-    def _finish(self):
-        if self.prog is not None:
-            self.prog.close()