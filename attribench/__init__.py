daff1b0e9f7ff43f1371dec5f13e4ad37f5a4128
Arne Gevaert <arne.gevaert@ugent.be>
2024-02-29 16:01:09 +0100

    Bump version 0.1.6 -> 0.1.7

--- attribench/__init__.py
+++ attribench/__init__.py
@@ -2,4 +2,4 @@ from ._attribution_method import AttributionMethod
 from ._method_factory import MethodFactory
 from ._model_factory import ModelFactory, BasicModelFactory
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
